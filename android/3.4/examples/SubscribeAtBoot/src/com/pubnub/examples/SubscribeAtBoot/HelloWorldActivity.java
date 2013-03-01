671907d2713adb1250988a6f7bd1cc77be582808
geremy <geremy@pubnub.com>
2013-03-01 15:37:55 -0800

    changing package name. Adding an activity to kickstart service on later versions of Android OS.

--- android/3.4/examples/SubscribeAtBoot/src/com/pubnub/examples/SubscribeAtBoot/HelloWorldActivity.java
+++ android/3.4/examples/SubscribeAtBoot/src/com/pubnub/examples/SubscribeAtBoot/HelloWorldActivity.java
@@ -10,6 +10,10 @@ public class HelloWorldActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        // In new versions of Android, the service may not be activated unless an
+        // associated activity is run at least once. This empty activity serves
+        // that purpose
+
         Intent serviceIntent = new Intent(this, PubnubService.class);
         startService(serviceIntent);
 
--- android/3.4/examples/SubscribeAtBoot/src/com/pubnub/examples/SubscribeAtBoot/PubnubService.java
+++ android/3.4/examples/SubscribeAtBoot/src/com/pubnub/examples/SubscribeAtBoot/PubnubService.java
@@ -1,5 +1,8 @@
 package com.pubnub.examples.SubscribeAtBoot;
 
+import android.content.Context;
+import android.os.Handler;
+import android.os.Message;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
@@ -7,9 +10,7 @@ import com.pubnub.api.Callback;
 import com.pubnub.api.Pubnub;
 import com.pubnub.api.PubnubException;
 
-import android.app.Notification;
-import android.app.NotificationManager;
-import android.app.PendingIntent;
+
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
@@ -18,90 +19,83 @@ import android.widget.Toast;
 
 public class PubnubService extends Service {
 
-	String channel = "hello_world";
-	Pubnub pubnub = new Pubnub("demo", "demo", false);
-	private int msgid = 0;
+    String channel = "hello_world";
+    Pubnub pubnub = new Pubnub("demo", "demo", false);
 
-	private void showNotification(String s) {
-		NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
-		Notification notification = new Notification(R.drawable.icon, s,
-				System.currentTimeMillis());
-		PendingIntent contentIntent = PendingIntent.getActivity(this, 0,
-				new Intent(this, PubnubService.class), 0);
-		notification.setLatestEventInfo(this, getText(R.string.app_name), s,
-				contentIntent);
-		notificationManager.notify(R.string.app_name, notification);
-	}
+    private final Handler handler = new Handler() {
+        public void handleMessage(Message msg) {
+            String pnMsg = msg.obj.toString();
 
-	private void notifyUser(Object message) {
+            final Toast toast = Toast.makeText(getApplicationContext(), pnMsg, Toast.LENGTH_SHORT);
+            toast.show();
 
-		try {
-			if (message instanceof JSONObject) {
-				final JSONObject obj = (JSONObject) message;
+            Handler handler = new Handler();
+            handler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    toast.cancel();
+                }
+            }, 200);
 
-				showNotification(String.valueOf(obj));
-				Log.i("Received msg : ", String.valueOf(obj));
+        }
+    };
 
-			} else if (message instanceof String) {
-				final String obj = (String) message;
+    private void notifyUser(Object message) {
 
-				showNotification(String.valueOf(obj));
-				Log.i("Received msg : ", obj.toString());
+        Message msg = handler.obtainMessage();
 
-			} else if (message instanceof JSONArray) {
-				final JSONArray obj = (JSONArray) message;
+        try {
+            final String obj = (String) message;
+            msg.obj = obj;
+            handler.sendMessage(msg);
+            Log.i("Received msg : ", obj.toString());
 
-				showNotification(String.valueOf(obj));
-				Log.i("Received msg : ", obj.toString());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
-			}
+    public void onCreate() {
+        super.onCreate();
+        Toast.makeText(this, "PubnubService created...", Toast.LENGTH_LONG).show();
+        Log.i("PUBNUB", "PubnubService created...");
+        try {
+            pubnub.subscribe(new String[]{channel}, new Callback() {
+                public void connectCallback(String channel) {
+                    notifyUser("CONNECT on channel:" + channel);
+                }
 
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
+                public void disconnectCallback(String channel) {
+                    notifyUser("DISCONNECT on channel:" + channel);
+                }
 
-	public void onCreate() {
-		super.onCreate();
-		Toast.makeText(this, "PubnubService created...", Toast.LENGTH_LONG).show();
-		Log.i("PUBNUB", "PubnubService created...");
-		try {
-			pubnub.subscribe(new String[] { channel }, new Callback() {
-				public void connectCallback(String channel) {
-					notifyUser("CONNECT on channel:" + channel);
-				}
+                public void reconnectCallback(String channel) {
+                    notifyUser("RECONNECT on channel:" + channel);
+                }
 
-				public void disconnectCallback(String channel) {
-					notifyUser("DISCONNECT on channel:" + channel);
-				}
+                public void successCallback(String channel, Object message) {
+                    notifyUser(channel + " " + message.toString());
+                }
 
-				public void reconnectCallback(String channel) {
-					notifyUser("RECONNECT on channel:" + channel);
-				}
+                public void errorCallback(String channel, Object message) {
+                    notifyUser(channel + " " + message.toString());
+                }
+            });
+        } catch (PubnubException e) {
 
-				public void successCallback(String channel, Object message) {
-					notifyUser(channel + " " + message.toString());
-				}
+        }
+    }
 
-				public void errorCallback(String channel, Object message) {
-					notifyUser(channel + " " + message.toString());
-				}
-			});
-		} catch (PubnubException e) {
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Toast.makeText(this, "PubnubService destroyed...", Toast.LENGTH_LONG).show();
+    }
 
-		}
-	}
-
-	@Override
-	public void onDestroy() {
-		super.onDestroy();
-		Toast.makeText(this, "PubnubService destroyed...", Toast.LENGTH_LONG).show();
-	}
-
-	@Override
-	public IBinder onBind(Intent intent) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO Auto-generated method stub
+        return null;
+    }
 
 }
