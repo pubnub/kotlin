b70deac712e48c6e1d36d3e67325824f91f16f15
Devendra <devendra@udgama.com>
2013-07-05 00:54:23 +0530

    adding logging support to pubnub cn1 lib

--- codenameone/src/com/pubnub/api/Logger.java
+++ codenameone/src/com/pubnub/api/Logger.java
@@ -1,34 +1,40 @@
 package com.pubnub.api;
 
-//import org.slf4j.LoggerFactory;
+import com.codename1.io.Log;
 
 class Cn1Logger {
-	
-	public Cn1Logger(Class c) {
-		
-	}
-	public void debug(String s) {
-		
-	}
-	public void trace(String s) {
-		
-	}
-	public void error(String s) {
-		
-	}
-	public void info(String s) {
-		
-	}
+
+    private Log log;
+
+    public Cn1Logger(Class c) {
+        Log log = Log.getInstance();
+    }
+
+    public void debug(String s) {
+        log.p(s, Log.DEBUG);
+    }
+
+    public void trace(String s) {
+        log.p(s,Log.INFO);
+    }
+
+    public void error(String s) {
+        log.p(s,Log.ERROR);
+    }
+
+    public void info(String s) {
+        log.p(s,Log.INFO);
+    }
 }
 
-
 class Logger extends AbstractLogger {
+
     private Class _class;
     private Cn1Logger log;
+
     public Logger(Class _class) {
         this._class = _class;
         this.log = new Cn1Logger(this._class);
-        this.log = null;
     }
 
     @Override
