f9ec26bbc9f35d2aa2ba930e8375cd414bb48a5c
Devendra <devendra@udgama.com>
2015-06-25 15:08:29 +0530

    changing jar names for non master branches

--- java/src1/com/pubnub/api/PubnubCoreShared.java
+++ java/src1/com/pubnub/api/PubnubCoreShared.java
@@ -2,6 +2,10 @@ package com.pubnub.api;
 
 import javax.crypto.Mac;
 import javax.crypto.spec.SecretKeySpec;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+
 import java.io.UnsupportedEncodingException;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
@@ -630,5 +634,257 @@ abstract class PubnubCoreShared extends PubnubCore {
 
     public void pamRevokeChannelGroup(String group, String auth_key, Callback callback) {
         pamGrantChannelGroup(group, auth_key, false, false, -1, callback);
+    }
+
+    /**
+     * Enable Push Notifications (Google Cloud Messaging)
+     * @param channel
+     *             Channel for which to enable push notifications
+     * @param gcmRegistrationId
+     *             Google Cloud Messaging registration id
+     */
+    public void enablePushNotificationsOnChannel(String channel, String gcmRegistrationId) {
+        enablePushNotificationsOnChannels(new String[]{channel}, gcmRegistrationId, null);
+    }
+
+    /**
+     * Enable Push Notifications (Google Cloud Messaging)
+     * @param channels
+     *             Channels for which to enable push notifications
+     * @param gcmRegistrationId
+     *             Google Cloud Messaging registration id
+     */
+    public void enablePushNotificationsOnChannels(String[] channels, String gcmRegistrationId) {
+        enablePushNotificationsOnChannels(channels, gcmRegistrationId, null);
+    }
+
+    /**
+     * Enable Push Notifications (Google Cloud Messaging)
+     * @param channel
+     *             Channel for which to enable push notifications
+     * @param gcmRegistrationId
+     *             Google Cloud Messaging registration id
+     * @param callback
+     *             Callback object
+     */
+    public void enablePushNotificationsOnChannel(String channel, String gcmRegistrationId, Callback callback) {
+        enablePushNotificationsOnChannels(new String[]{channel}, gcmRegistrationId, callback);
+    }
+
+
+    /**
+     * Enable Push Notifications (Google Cloud Messaging)
+     * @param channels
+     *             Channels for which to enable push notifications
+     * @param gcmRegistrationId
+     *             Google Cloud Messaging registration id
+     * @param callback
+     *             Callback object
+     */
+    public void enablePushNotificationsOnChannels(final String[] channels, String gcmRegistrationId, final Callback callback) {
+        final Callback cb = getWrappedCallback(callback);
+
+        Hashtable parameters = PubnubUtil.hashtableClone(params);
+        String[] urlargs = null;
+        urlargs = new String[]{ getPubnubUrl(), "v1", "push", "sub-key",
+                this.SUBSCRIBE_KEY, "devices", gcmRegistrationId
+        };
+
+        parameters.put("type", "gcm");
+        parameters.put("add", PubnubUtil.joinString(channels, ","));
+
+        HttpRequest hreq = new HttpRequest(urlargs, parameters,
+                new ResponseHandler() {
+            public void handleResponse(HttpRequest hreq, String response) {
+                JSONArray jsarr;
+                try {
+                    jsarr = new JSONArray(response);
+                } catch (JSONException e) {
+                    handleError(hreq,
+                            PubnubError.getErrorObject(PubnubError.PNERROBJ_INVALID_JSON, 1, response));
+                    return;
+                }
+                cb.successCallback("", jsarr);
+            }
+
+            public void handleError(HttpRequest hreq, PubnubError error) {
+                cb.errorCallback("", error);
+                return;
+            }
+        });
+
+        _request(hreq, nonSubscribeManager);
+    }
+
+    /**
+     * Disable Push Notifications (Google Cloud Messaging)
+     * @param channel
+     *             Channel for which to disable push notifications
+     * @param gcmRegistrationId
+     *             Google Cloud Messaging registration id
+     */
+    public void disablePushNotificationsOnChannel(String channel, String gcmRegistrationId) {
+        disablePushNotificationsOnChannels(new String[]{channel}, gcmRegistrationId, null);
+    }
+
+    /**
+     * Disable Push Notifications (Google Cloud Messaging)
+     * @param channels
+     *             Channels for which to disable push notifications
+     * @param gcmRegistrationId
+     *             Google Cloud Messaging registration id
+     */
+    public void disablePushNotificationsOnChannels(String[] channels, String gcmRegistrationId) {
+        disablePushNotificationsOnChannels(channels, gcmRegistrationId, null);
+    }
+
+    /**
+     * Disable Push Notifications (Google Cloud Messaging)
+     * @param channel
+     * @param gcmRegistrationId
+     * @param callback
+     */
+    public void disablePushNotificationsOnChannel(String channel, String gcmRegistrationId, Callback callback) {
+        disablePushNotificationsOnChannels(new String[]{channel}, gcmRegistrationId, callback);
+    }
+
+    /**
+     * Disable Push Notifications (Google Cloud Messaging)
+     * @param channel
+     * @param callback
+     */
+    /**
+     * @param channels
+     *             Channels for which to disable push notifications
+     * @param gcmRegistrationId
+     *             Google Cloud Messaging registration id
+     * @param callback
+     *             Callback object
+     */
+    public void disablePushNotificationsOnChannels(final String[] channels, String gcmRegistrationId, final Callback callback) {
+        final Callback cb = getWrappedCallback(callback);
+
+
+        Hashtable parameters = PubnubUtil.hashtableClone(params);
+        String[] urlargs = null;
+        urlargs = new String[]{ getPubnubUrl(), "v1", "push", "sub-key",
+                this.SUBSCRIBE_KEY, "devices", gcmRegistrationId
+        };
+
+
+        parameters.put("type", "gcm");
+        parameters.put("remove", PubnubUtil.joinString(channels, ","));
+
+        HttpRequest hreq = new HttpRequest(urlargs, parameters,
+                new ResponseHandler() {
+            public void handleResponse(HttpRequest hreq, String response) {
+                JSONArray jsarr;
+                try {
+                    jsarr = new JSONArray(response);
+                } catch (JSONException e) {
+                    handleError(hreq,
+                            PubnubError.getErrorObject(PubnubError.PNERROBJ_INVALID_JSON, 1, response));
+                    return;
+                }
+                cb.successCallback("", jsarr);
+            }
+
+            public void handleError(HttpRequest hreq, PubnubError error) {
+                cb.errorCallback("", error);
+                return;
+            }
+        });
+
+        _request(hreq, nonSubscribeManager);
+    }
+
+
+    /**
+     * Get channels for which push notification is enabled (Google Cloud Messaging)
+     * @param gcmRegistrationId
+     *             Google Cloud Messaging registration id
+     * @param callback
+     *             Callback object
+     */
+    public void requestPushNotificationEnabledChannelsForDeviceRegistrationId(String gcmRegistrationId, final Callback callback) {
+        final Callback cb = getWrappedCallback(callback);
+        Hashtable parameters = PubnubUtil.hashtableClone(params);
+        String[] urlargs = null;
+        urlargs = new String[]{ getPubnubUrl(), "v1", "push", "sub-key",
+                this.SUBSCRIBE_KEY, "devices", gcmRegistrationId
+        };
+
+
+        parameters.put("type", "gcm");
+
+        HttpRequest hreq = new HttpRequest(urlargs, parameters,
+                new ResponseHandler() {
+            public void handleResponse(HttpRequest hreq, String response) {
+                JSONArray jsarr;
+                try {
+                    jsarr = new JSONArray(response);
+                } catch (JSONException e) {
+                    handleError(hreq,
+                            PubnubError.getErrorObject(PubnubError.PNERROBJ_INVALID_JSON, 1, response));
+                    return;
+                }
+                cb.successCallback("", jsarr);
+            }
+
+            public void handleError(HttpRequest hreq, PubnubError error) {
+                cb.errorCallback("", error);
+                return;
+            }
+        });
+        _request(hreq, nonSubscribeManager);
+    }
+
+    /**
+     * Disable push notifications for all channels (Google Cloud Messaging)
+     * @param gcmRegistrationId
+     *             Google Cloud Messaging registration id
+     */
+    public void removeAllPushNotificationsForDeviceRegistrationId(String gcmRegistrationId) {
+        removeAllPushNotificationsForDeviceRegistrationId(gcmRegistrationId, null);
+    }
+
+    /**
+     * Disable push notifications for all channels (Google Cloud Messaging)
+     * @param gcmRegistrationId
+     *             Google Cloud Messaging registration id
+     * @param callback
+     *             Callback object
+     */
+    public void removeAllPushNotificationsForDeviceRegistrationId(String gcmRegistrationId, final Callback callback) {
+        final Callback cb = getWrappedCallback(callback);
+        Hashtable parameters = PubnubUtil.hashtableClone(params);
+        String[] urlargs = null;
+        urlargs = new String[]{ getPubnubUrl(), "v1", "push", "sub-key",
+                this.SUBSCRIBE_KEY, "devices", gcmRegistrationId, "remove"
+        };
+
+
+        parameters.put("type", "gcm");
+
+        HttpRequest hreq = new HttpRequest(urlargs, parameters,
+                new ResponseHandler() {
+            public void handleResponse(HttpRequest hreq, String response) {
+                JSONArray jsarr;
+                try {
+                    jsarr = new JSONArray(response);
+                } catch (JSONException e) {
+                    handleError(hreq,
+                            PubnubError.getErrorObject(PubnubError.PNERROBJ_INVALID_JSON, 1, response));
+                    return;
+                }
+                cb.successCallback("", jsarr);
+            }
+
+            public void handleError(HttpRequest hreq, PubnubError error) {
+                cb.errorCallback("", error);
+                return;
+            }
+        });
+        _request(hreq, nonSubscribeManager);
     }
 }
