c3bc01c5c78596ddd96e88dc90241833d7c4d623
Devendra <devendra@udgama.com>
2015-08-07 19:28:11 +0530

    documentation changes

--- java/srcPubnubApi/com/pubnub/api/PubnubSyncInterfacePush.java
+++ java/srcPubnubApi/com/pubnub/api/PubnubSyncInterfacePush.java
@@ -3,14 +3,37 @@ package com.pubnub.api;
 /**
  * Created by work1 on 06/08/15.
  */
-public interface PubnubSyncInterfacePush {
-
+interface PubnubSyncInterfacePush {
+    /**
+     *
+     * @param channel
+     * @param gcmRegistrationId
+     * @return
+     */
     Object	enablePushNotificationsOnChannel(String channel, String gcmRegistrationId);
 
+    /**
+     *
+     * @param channels
+     * @param gcmRegistrationId
+     * @return
+     */
     Object	enablePushNotificationsOnChannels(String[] channels, String gcmRegistrationId);
 
+    /**
+     *
+     * @param channel
+     * @param gcmRegistrationId
+     * @return
+     */
     Object	disablePushNotificationsOnChannel(String channel, String gcmRegistrationId);
 
+    /**
+     *
+     * @param channels
+     * @param gcmRegistrationId
+     * @return
+     */
     Object	disablePushNotificationsOnChannels(String[] channels, String gcmRegistrationId);
 
 }
