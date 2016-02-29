888d0a6dc867b474edf06b7a9682e8797eb884d8
dhleong <falcone88@gmail.com>
2016-02-29 09:44:23 -0500

    Replace commons lang method with own implementation
    Includes tests

--- java/src1/com/pubnub/api/PubnubUtil.java
+++ java/src1/com/pubnub/api/PubnubUtil.java
@@ -1,39 +1,130 @@
 package com.pubnub.api;
 
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.OutputStreamWriter;
-import java.io.UnsupportedEncodingException;
-import java.net.URLEncoder;
-
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import org.apache.commons.lang3.StringEscapeUtils;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.util.Locale;
 
 
 /**
  * PubnubUtil class provides utility methods like urlEncode etc
- * 
+ *
  * @author Pubnub
- * 
+ *
  */
 public class PubnubUtil extends PubnubUtilCore {
+    static final char[] HEX_DIGITS = new char[]{'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
 
     public static String escapeJava(String a) {
-        return StringEscapeUtils.escapeJava(a);
+//        return StringEscapeUtils.escapeJava(a);
+        StringBuilder result = new StringBuilder(a);
+        int len = a.length();
+        for (int i=0, j=0; i < len; i++, j++) {
+            final char in = a.charAt(i);
+            final char escaped = escapeJavaChar(in);
+            if (escaped != 0) {
+                result.insert(j++, '\\');
+                result.setCharAt(j, escaped);
+            } else {
+                int point = a.codePointAt(i);
+                j = unicodeEscape(result, point, j);
+                if (point > '\uffff') {
+                    // multi-char
+                    i++;
+                }
+            }
+        }
+        return result.toString();
     }
 
+    /**
+     * @return 0 if the char need not be escaped,
+     *  else the character that should replace `in`
+     *  after being escaped with a backslash.
+     */
+    private static char escapeJavaChar(char in) {
+        if (in == '"' || in == '\\') {
+            return in;
+        }
+
+        // JAVA_CTRL_CHARS_ESCAPE
+        if (in == '\b') {
+            return 'b';
+        } else if (in == '\n') {
+            return 'n';
+        } else if (in == '\t') {
+            return 't';
+        } else if (in == '\f') {
+            return 'f';
+        } else if (in == '\r') {
+            return 'r';
+        }
+
+        return 0;
+    }
+
+    /**
+     * Given a char, and its location in a StringBuilder,
+     *  replace it with an escaped unicode if necessary
+     * @return The new index into StringBuilder
+     */
+    private static int unicodeEscape(StringBuilder result, int in, int index) {
+        // utf8 within [32, 127] are fine
+        if (in >= 32 && in <= 127) {
+            return index;
+        }
+
+        if (in > '\uffff') {
+            final char[] surrogatePair = Character.toChars(in);
+            final String first = hex(surrogatePair[0]);
+            final String second = hex(surrogatePair[1]);
+
+            // ensure we have capacity to replace 2 chars
+            // (IE a surrogate pair) with \\u + first + \\u + second
+            result.ensureCapacity(result.capacity() + 2
+                    + first.length() + second.length());
+            result.delete(index, index + 2);
+            result.insert(index++, '\\');
+            result.insert(index++, 'u');
+            result.insert(index, first);
+            index += first.length();
+
+            result.insert(index++, '\\');
+            result.insert(index++, 'u');
+            result.insert(index, second);
+            index += second.length() - 1;
+        } else {
+            // ensure we have capacity to replace 1 char with 6
+            result.ensureCapacity(result.capacity() + 5);
+            result.deleteCharAt(index);
+            result.insert(index++, '\\');
+            result.insert(index++, 'u');
+            result.insert(index++, HEX_DIGITS[in >> 12 & 15]);
+            result.insert(index++, HEX_DIGITS[in >> 8 & 15]);
+            result.insert(index++, HEX_DIGITS[in >> 4 & 15]);
+            result.insert(index, HEX_DIGITS[in & 15]);
+        }
+
+        return index;
+    }
+
+    private static String hex(int codepoint) {
+        return Integer.toHexString(codepoint).toUpperCase(Locale.ENGLISH);
+    }
+
+
     public static String stringEscapeSlashes(String s, String a, String b) {
-		return s.replace(a, b);
+        return s.replace(a, b);
     }
     public static String stringReplaceAll(String s, String a, String b) {
-		return s.replaceAll(a, b);
+        return s.replaceAll(a, b);
     }
     /**
      * Returns encoded String
-     * 
+     *
      * @param sUrl
      *            , input string
      * @return , encoded string
@@ -52,7 +143,7 @@ public class PubnubUtil extends PubnubUtilCore {
 
     /**
      * Returns encoded String
-     * 
+     *
      * @param sUrl
      *            , input string
      * @return , encoded string
@@ -67,10 +158,10 @@ public class PubnubUtil extends PubnubUtilCore {
 
     /**
      * Convert input String to JSONObject, JSONArray, or String
-     * 
+     *
      * @param str
      *            JSON data in string format
-     * 
+     *
      * @return JSONArray or JSONObject or String
      */
     static Object stringToJSON(String str) {
@@ -93,3 +184,4 @@ public class PubnubUtil extends PubnubUtilCore {
         return str;
     }
 }
+
