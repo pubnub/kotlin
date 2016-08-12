8e30b693c5a60f6197a8178b2785382aed15cf06
Marcelo Marques Inacio <marceloinacio@hotmail.com>
2016-08-12 11:05:40 -0400

    Exponential backoff

--- src/main/java/com/pubnub/api/managers/ReconnectionManager.java
+++ src/main/java/com/pubnub/api/managers/ReconnectionManager.java
@@ -8,6 +8,7 @@ import com.pubnub.api.models.consumer.PNStatus;
 import com.pubnub.api.models.consumer.PNTimeResult;
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.Calendar;
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -15,10 +16,15 @@ import java.util.TimerTask;
 @Slf4j
 public class ReconnectionManager {
 
-    private static final int INTERVAL = 3000;
+    private static final int INTERVAL = 3;
 
     private ReconnectionCallback callback;
     private PubNub pubnub;
+    private static final int MINEXPONENTIALBACKOFF = 1;
+    private static final int MAXEXPONENTIALBACKOFF = 32;
+    private int timerInterval;
+    private int connectionErrors = 1;
+    private static final int MILLISECONDS = 1000;
 
     /**
      * Timer for heartbeat operations.
@@ -49,13 +55,27 @@ public class ReconnectionManager {
         stopHeartbeatTimer();
 
         timer = new Timer();
+
+        if (pubnub.getConfiguration().getReconnectionPolicy() == PNReconnectionPolicy.EXPONENTIAL) {
+            timerInterval = (int) (Math.pow(2, connectionErrors) - 1);
+            if (timerInterval > MAXEXPONENTIALBACKOFF) {
+                timerInterval = MINEXPONENTIALBACKOFF;
+                connectionErrors = 1;
+                log.debug("timerInterval > MAXEXPONENTIALBACKOFF at: " + Calendar.getInstance().getTime().toString());
+            } else if (timerInterval < 1) {
+                timerInterval = MINEXPONENTIALBACKOFF;
+            }
+            log.debug("timerInterval = " + String.valueOf(timerInterval) + " at: " + Calendar.getInstance().getTime().toString());
+        } else {
+            timerInterval = INTERVAL;
+        }
+
         timer.schedule(new TimerTask() {
             @Override
             public void run() {
                 callTime();
             }
-        }, 0, INTERVAL);
-
+        }, timerInterval * MILLISECONDS, timerInterval * MILLISECONDS);
     }
 
     private void stopHeartbeatTimer() {
@@ -70,11 +90,16 @@ public class ReconnectionManager {
             @Override
             public void onResponse(PNTimeResult result, PNStatus status) {
                 if (!status.isError()) {
+                    connectionErrors = 1;
                     stopHeartbeatTimer();
                     callback.onReconnection();
+                } else if (pubnub.getConfiguration().getReconnectionPolicy() == PNReconnectionPolicy.EXPONENTIAL) {
+                    log.debug("callTime() at: " + Calendar.getInstance().getTime().toString());
+                    stopHeartbeatTimer();
+                    connectionErrors++;
+                    registerHeartbeatTimer();
                 }
             }
         });
     }
-
 }
