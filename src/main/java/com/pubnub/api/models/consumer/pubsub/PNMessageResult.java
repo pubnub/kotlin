theshrestha/java

associated_channel_on_subscribe

src/main/java/com/pubnub/api/models/consumer/pubsub/PNMessageResult.java
@@ -10,8 +10,14 @@ public class PNMessageResult {
 
     private JsonNode message;
 
+    @Deprecated
     private String subscribedChannel;
+    @Deprecated
     private String actualChannel;
+
+    private String associatedChannel;
+    private String associatedChannelGroup;
+
     private Long timetoken;
     private Object userMetadata;
 

src/main/java/com/pubnub/api/models/consumer/pubsub/PNPresenceEventResult.java
@@ -15,8 +15,14 @@ public class PNPresenceEventResult {
     private Integer occupancy;
     private JsonNode state;
 
+    @Deprecated
     private String subscribedChannel;
+    @Deprecated
     private String actualChannel;
+
+    private String associatedChannel;
+    private String associatedChannelGroup;
+
     private Long timetoken;
     private Object userMetadata;
 

src/main/java/com/pubnub/api/workers/SubscribeMessageWorker.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.pubnub.api.PubNub;
 import com.pubnub.api.PubNubException;
+import com.pubnub.api.PubNubUtil;
 import com.pubnub.api.enums.PNOperationType;
 import com.pubnub.api.enums.PNStatusCategory;
 import com.pubnub.api.managers.ListenerManager;
@@ -120,13 +121,30 @@ public class SubscribeMessageWorker implements Runnable {
             subscriptionMatch = null;
         }
 
-        if (message.getChannel().contains("-pnpres")) {
+        if (message.getChannel().endsWith("-pnpres")) {
             PresenceEnvelope presencePayload = mapper.convertValue(message.getPayload(), PresenceEnvelope.class);
+
+            String associatedChannel = message.getChannel();
+            String associatedChannelGroup = message.getSubscriptionMatch();
+
+            if (associatedChannel != null) {
+                associatedChannel = PubNubUtil.replaceLast(associatedChannel, "-pnpres", "");
+            }
+            if (associatedChannelGroup != null) {
+                associatedChannelGroup = PubNubUtil.replaceLast(associatedChannelGroup, "-pnpres", "");
+            }
 
             PNPresenceEventResult pnPresenceEventResult = PNPresenceEventResult.builder()
                     .event(presencePayload.getAction())
+
+                    // deprecated
                     .actualChannel((subscriptionMatch != null) ? channel : null)
                     .subscribedChannel(subscriptionMatch != null ? subscriptionMatch : channel)
+                    // deprecated
+
+                    .associatedChannel(associatedChannel)
+                    .associatedChannelGroup(associatedChannelGroup)
+
                     .state(presencePayload.getData())
                     .timetoken(publishMetaData.getPublishTimetoken())
                     .occupancy(presencePayload.getOccupancy())
@@ -144,8 +162,12 @@ public class SubscribeMessageWorker implements Runnable {
 
             PNMessageResult pnMessageResult = PNMessageResult.builder()
                     .message(extractedMessage)
+                    // deprecated
                     .actualChannel((subscriptionMatch != null) ? channel : null)
                     .subscribedChannel(subscriptionMatch != null ? subscriptionMatch : channel)
+                    // deprecated
+                    .associatedChannel(channel)
+                    .associatedChannelGroup(subscriptionMatch)
                     .timetoken(publishMetaData.getPublishTimetoken())
                     .build();
 

pubnub/kotlin

src/main/java/com/pubnub/api/models/consumer/pubsub/PNMessageResult.java
@@ -10,9 +10,16 @@ public class PNMessageResult {
 
     private JsonNode message;
 
+    @Deprecated
     private String subscribedChannel;
+    @Deprecated
     private String actualChannel;
+
+    private String channel;
+    private String channelGroup;
+
     private Long timetoken;
     private Object userMetadata;
 
 }
+

src/main/java/com/pubnub/api/models/consumer/pubsub/PNPresenceEventResult.java
@@ -15,8 +15,14 @@ public class PNPresenceEventResult {
     private Integer occupancy;
     private JsonNode state;
 
+    @Deprecated
     private String subscribedChannel;
+    @Deprecated
     private String actualChannel;
+
+    private String channel;
+    private String channelGroup;
+
     private Long timetoken;
     private Object userMetadata;
 

src/main/java/com/pubnub/api/workers/SubscribeMessageWorker.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.pubnub.api.PubNub;
 import com.pubnub.api.PubNubException;
+import com.pubnub.api.PubNubUtil;
 import com.pubnub.api.enums.PNOperationType;
 import com.pubnub.api.enums.PNStatusCategory;
 import com.pubnub.api.managers.ListenerManager;
@@ -120,13 +121,27 @@ public class SubscribeMessageWorker implements Runnable {
             subscriptionMatch = null;
         }
 
-        if (message.getChannel().contains("-pnpres")) {
+        if (message.getChannel().endsWith("-pnpres")) {
             PresenceEnvelope presencePayload = mapper.convertValue(message.getPayload(), PresenceEnvelope.class);
+
+            String associatedChannel = message.getChannel();
+            String associatedChannelGroup = message.getSubscriptionMatch();
+
+            if (associatedChannel != null) {
+                associatedChannel = PubNubUtil.replaceLast(associatedChannel, "-pnpres", "");
+            }
+            if (associatedChannelGroup != null) {
+                associatedChannelGroup = PubNubUtil.replaceLast(associatedChannelGroup, "-pnpres", "");
+            }
 
             PNPresenceEventResult pnPresenceEventResult = PNPresenceEventResult.builder()
                     .event(presencePayload.getAction())
+                    // deprecated
                     .actualChannel((subscriptionMatch != null) ? channel : null)
                     .subscribedChannel(subscriptionMatch != null ? subscriptionMatch : channel)
+                    // deprecated
+                    .channel(associatedChannel)
+                    .channelGroup(associatedChannelGroup)
                     .state(presencePayload.getData())
                     .timetoken(publishMetaData.getPublishTimetoken())
                     .occupancy(presencePayload.getOccupancy())
@@ -144,8 +159,12 @@ public class SubscribeMessageWorker implements Runnable {
 
             PNMessageResult pnMessageResult = PNMessageResult.builder()
                     .message(extractedMessage)
+                    // deprecated
                     .actualChannel((subscriptionMatch != null) ? channel : null)
                     .subscribedChannel(subscriptionMatch != null ? subscriptionMatch : channel)
+                    // deprecated
+                    .channel(channel)
+                    .channelGroup(subscriptionMatch)
                     .timetoken(publishMetaData.getPublishTimetoken())
                     .build();
 