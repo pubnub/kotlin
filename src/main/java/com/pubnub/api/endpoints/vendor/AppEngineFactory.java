68c1647f9119da442a2926f8eeac6d0c5da9e7e4
Max Presman <max@pubnub.com>
2017-08-08 08:30:25 -0700

    move signing into utils

--- src/main/java/com/pubnub/api/endpoints/vendor/AppEngineFactory.java
+++ src/main/java/com/pubnub/api/endpoints/vendor/AppEngineFactory.java
@@ -1,20 +1,11 @@
 package com.pubnub.api.endpoints.vendor;
 
 import com.pubnub.api.PubNub;
-import com.pubnub.api.PubNubException;
 import com.pubnub.api.PubNubUtil;
-
-import java.io.IOException;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.util.HashMap;
-import java.util.Map;
-
 import lombok.extern.java.Log;
 import okhttp3.Call;
 import okhttp3.Callback;
 import okhttp3.Headers;
-import okhttp3.HttpUrl;
 import okhttp3.MediaType;
 import okhttp3.Protocol;
 import okhttp3.Request;
@@ -23,6 +14,10 @@ import okhttp3.ResponseBody;
 import okio.BufferedSink;
 import okio.BufferedSource;
 import okio.Okio;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 @Log
 public class AppEngineFactory implements Call {
@@ -41,7 +36,7 @@ public class AppEngineFactory implements Call {
 
     @Override
     public Response execute() throws IOException {
-        request = signIfNecessary(request); // We need this since we bypass the signature interceptor
+        request = PubNubUtil.requestSigner(request, pubNub.getConfiguration(), pubNub.getTimestamp());
 
         URL url = request.url().url();
         final HttpURLConnection connection = (HttpURLConnection) url.openConnection();
@@ -93,53 +88,6 @@ public class AppEngineFactory implements Call {
                 })
                 .build();
         return response;
-    }
-
-    // TODO: This should probably go in a util file so we don't have copied code from SignatureInterceptor
-    public Request signIfNecessary(Request request) {
-        // only sign if we have a secret key in place.
-        if (this.pubNub.getConfiguration().getSecretKey() == null) {
-            return request;
-        }
-
-        HttpUrl url = request.url();
-        String requestURL = url.encodedPath();
-        int timestamp = this.pubNub.getTimestamp();
-        Map<String, String> queryParams = new HashMap<>();
-        String signature = "";
-
-        for (String queryKey: url.queryParameterNames()) {
-            queryParams.put(queryKey, url.queryParameter(queryKey));
-        }
-
-        queryParams.put("timestamp", String.valueOf(timestamp));
-
-        String signInput = pubNub.getConfiguration().getSubscribeKey() + "\n"
-                + pubNub.getConfiguration().getPublishKey() + "\n";
-
-        if (requestURL.startsWith("/v1/auth/audit")) {
-            signInput += "audit" + "\n";
-        } else if (requestURL.startsWith("/v1/auth/grant")) {
-            signInput += "grant" + "\n";
-        } else {
-            signInput += requestURL + "\n";
-        }
-
-        signInput += PubNubUtil.preparePamArguments(queryParams);
-
-        try {
-            signature = PubNubUtil.signSHA256(pubNub.getConfiguration().getSecretKey(), signInput);
-        } catch (PubNubException e) {
-            log.warning("signature failed on SignatureInterceptor: " + e.toString());
-        }
-
-        HttpUrl rebuiltUrl = url.newBuilder()
-                .addQueryParameter("timestamp", String.valueOf(timestamp))
-                .addQueryParameter("signature", signature)
-                .build();
-
-        request = request.newBuilder().url(rebuiltUrl).build();
-        return request;
     }
 
     @Override
