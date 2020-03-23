e7a7e38c368ae22fa10387a0401f4358a331c49c
azurpn <azur.haljeta@pubnub.com>
2020-03-23 21:21:17 +0100

    Implement the subscribe loop

---
+++ src/main/kotlin/com/pubnub/api/workers/SubscribeMessageWorker.kt
@@ -0,0 +1,205 @@
+package com.pubnub.api.workers
+
+import com.google.gson.JsonElement
+import com.pubnub.api.PubNub
+import com.pubnub.api.PubNubException
+import com.pubnub.api.PubNubUtil
+import com.pubnub.api.enums.PNOperationType
+import com.pubnub.api.enums.PNStatusCategory
+import com.pubnub.api.managers.DuplicationManager
+import com.pubnub.api.managers.ListenerManager
+import com.pubnub.api.models.consumer.PNStatus
+import com.pubnub.api.models.consumer.pubsub.BasePubSubResult
+import com.pubnub.api.models.consumer.pubsub.PNMessageResult
+import com.pubnub.api.models.consumer.pubsub.PNPresenceEventResult
+import com.pubnub.api.models.consumer.pubsub.PNSignalResult
+import com.pubnub.api.models.server.PresenceEnvelope
+import com.pubnub.api.models.server.SubscribeMessage
+import com.pubnub.api.vendor.Crypto
+import java.util.concurrent.LinkedBlockingQueue
+
+class SubscribeMessageWorker(
+    val pubnub: PubNub,
+    val listenerManager: ListenerManager,
+    val queue: LinkedBlockingQueue<SubscribeMessage>,
+    val duplicationManager: DuplicationManager
+) : Runnable {
+
+    private var running = false
+
+    companion object {
+        private const val TYPE_MESSAGE = 0
+        private const val TYPE_SIGNAL = 1
+        private const val TYPE_OBJECT = 2
+        private const val TYPE_MESSAGE_ACTION = 3
+    }
+
+    override fun run() {
+        takeMessage()
+    }
+
+    private fun takeMessage() {
+        running = true
+
+        while (running) {
+            try {
+                processIncomingPayload(queue.take())
+            } catch (e: InterruptedException) {
+                running = false
+                print("take message interrupted!")
+            }
+        }
+    }
+
+    private fun processIncomingPayload(message: SubscribeMessage) {
+        if (message.channel == null) {
+            return
+        }
+
+        val channel = message.channel
+        var subscriptionMatch = message.subscriptionMatch
+        val publishMetaData = message.publishMetaData
+
+        if (channel == subscriptionMatch) {
+            subscriptionMatch = null
+        }
+
+        if (pubnub.configuration.dedupOnSubscribe) {
+            if (duplicationManager.isDuplicate(message)) {
+                return
+            } else {
+                duplicationManager.addEntry(message)
+            }
+        }
+
+        if (message.channel.endsWith("-pnpres")) {
+            val presencePayload = pubnub.mapper.convertValue(message.payload, PresenceEnvelope::class.java)
+            val strippedPresenceChannel = PubNubUtil.replaceLast(channel, "-pnpres", "")
+            val strippedPresenceSubscription = subscriptionMatch?.let {
+                PubNubUtil.replaceLast(it, "-pnpres", "")
+            }
+
+            val isHereNowRefresh = message.payload?.asJsonObject?.get("here_now_refresh")
+
+            listenerManager.announce(
+                PNPresenceEventResult(
+                    event = presencePayload.action,
+                    uuid = presencePayload.uuid,
+                    timestamp = presencePayload.timestamp,
+                    occupancy = presencePayload.occupancy,
+                    state = presencePayload.data,
+                    channel = strippedPresenceChannel,
+                    subscription = strippedPresenceSubscription,
+                    timetoken = publishMetaData?.publishTimetoken,
+                    join = getDelta(message.payload?.asJsonObject?.get("join")),
+                    leave = getDelta(message.payload?.asJsonObject?.get("leave")),
+                    timeout = getDelta(message.payload?.asJsonObject?.get("timeout")),
+                    hereNowRefresh = isHereNowRefresh != null && isHereNowRefresh.asBoolean
+                )
+            )
+        } else {
+            val extractedMessage = processMessage(message)
+
+            if (extractedMessage == null) {
+                println("unable to parse payload on #processIncomingMessages")
+            }
+
+            val result = BasePubSubResult(
+                channel = channel,
+                subscription = subscriptionMatch,
+                timetoken = publishMetaData?.publishTimetoken,
+                userMetadata = message.userMetadata,
+                publisher = message.issuingClientId
+            )
+
+            when (message.type) {
+                null -> {
+                    listenerManager.announce(PNMessageResult(result, extractedMessage!!))
+                }
+                TYPE_MESSAGE -> {
+                    listenerManager.announce(PNMessageResult(result, extractedMessage!!))
+                }
+                TYPE_SIGNAL -> {
+                    listenerManager.announce(PNSignalResult(result, extractedMessage!!))
+                }
+                TYPE_OBJECT -> {
+
+                }
+            }
+
+
+        }
+
+    }
+
+    private fun processMessage(subscribeMessage: SubscribeMessage): JsonElement? {
+        val input = subscribeMessage.payload
+
+        // if we do not have a crypto key, there is no way to process the node; let's return.
+        if (!pubnub.configuration.isCipherKeyValid()) {
+            return input
+        }
+
+        // if the message couldn't possibly be encrypted in the first place, there is no way to process the node;
+        // let's return.
+        if (!subscribeMessage.supportsEncryption()) {
+            return input
+        }
+
+        val crypto = Crypto(pubnub.configuration.cipherKey)
+
+        var inputText = pubnub.mapper.elementToString(input, "pn_other")?.let { it }
+        if (inputText == null) {
+            inputText = pubnub.mapper.elementToString(input)
+        }
+
+        val outputText =
+            try {
+                crypto.decrypt(inputText!!)
+            } catch (e: PubNubException) {
+                val pnStatus = PNStatus(
+                    PNStatusCategory.PNMalformedResponseCategory,
+                    true,
+                    PNOperationType.PNSubscribeOperation,
+                    e
+                )
+                listenerManager.announce(pnStatus)
+                return null
+            }
+
+        var outputObject =
+            try {
+                pubnub.mapper.fromJson(outputText, JsonElement::class.java)
+            } catch (e: PubNubException) {
+                val pnStatus = PNStatus(
+                    PNStatusCategory.PNMalformedResponseCategory,
+                    true,
+                    PNOperationType.PNSubscribeOperation,
+                    e
+                )
+                listenerManager.announce(pnStatus)
+                return null
+            }
+
+        if (pubnub.mapper.isJsonObject(input!!) && pubnub.mapper.hasField(input, "pn_other")) {
+            val objectNode = pubnub.mapper.getAsObject(input)
+            pubnub.mapper.putOnObject(objectNode, "pn_other", outputObject)
+            outputObject = objectNode
+        }
+
+        return outputObject
+    }
+
+    private fun getDelta(delta: JsonElement?): List<String> {
+        val list = mutableListOf<String>()
+        delta?.let {
+            it.asJsonArray.forEach { item: JsonElement? ->
+                item?.let {
+                    list.add(it.asString)
+                }
+            }
+        }
+        return list
+    }
+
+}
