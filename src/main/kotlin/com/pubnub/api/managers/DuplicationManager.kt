25ee177bb3844f0019fd10ec9f6628b91648ba9b
azurpn <azur.haljeta@pubnub.com>
2020-03-23 21:22:46 +0100

    Add DuplicationManager

---
+++ src/main/kotlin/com/pubnub/api/managers/DuplicationManager.kt
@@ -0,0 +1,29 @@
+package com.pubnub.api.managers
+
+import com.pubnub.api.PNConfiguration
+import com.pubnub.api.models.server.SubscribeMessage
+
+class DuplicationManager(
+    private val config: PNConfiguration
+) {
+
+    private val hashHistory: ArrayList<String> = ArrayList()
+
+    private fun getKey(message: SubscribeMessage) =
+        with(message) {
+            "${publishMetaData?.publishTimetoken}-${payload.hashCode()}"
+        }
+
+
+    fun isDuplicate(message: SubscribeMessage) = hashHistory.contains(getKey(message))
+
+    fun addEntry(message: SubscribeMessage) {
+        if (hashHistory.size >= config.maximumMessagesCacheSize) {
+            hashHistory.removeAt(0)
+        }
+        hashHistory.add(getKey(message))
+    }
+
+    fun clearHistory() = hashHistory.clear()
+
+}
