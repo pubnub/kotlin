ffc5ad546e82322bf7b2dd4ccda372b53a734f1c
azurqsd <azur.haljeta@qsd.ba>
2020-05-23 19:12:22 +0200

    Improve SubscribeCallback modularity

--- src/main/kotlin/com/pubnub/api/callbacks/SubscribeCallback.kt
+++ src/main/kotlin/com/pubnub/api/callbacks/SubscribeCallback.kt
@@ -13,17 +13,17 @@ import com.pubnub.api.models.consumer.pubsub.objects.PNUserResult
 abstract class SubscribeCallback {
     abstract fun status(pubnub: PubNub, pnStatus: PNStatus)
 
-    abstract fun message(pubnub: PubNub, pnMessageResult: PNMessageResult)
+    open fun message(pubnub: PubNub, pnMessageResult: PNMessageResult) {}
 
-    abstract fun presence(pubnub: PubNub, pnPresenceEventResult: PNPresenceEventResult)
+    open fun presence(pubnub: PubNub, pnPresenceEventResult: PNPresenceEventResult) {}
 
-    abstract fun signal(pubnub: PubNub, pnSignalResult: PNSignalResult)
+    open fun signal(pubnub: PubNub, pnSignalResult: PNSignalResult) {}
 
-    abstract fun user(pubnub: PubNub, pnUserResult: PNUserResult)
+    open fun user(pubnub: PubNub, pnUserResult: PNUserResult) {}
 
-    abstract fun space(pubnub: PubNub, pnSpaceResult: PNSpaceResult)
+    open fun space(pubnub: PubNub, pnSpaceResult: PNSpaceResult) {}
 
-    abstract fun membership(pubnub: PubNub, pnMembershipResult: PNMembershipResult)
+    open fun membership(pubnub: PubNub, pnMembershipResult: PNMembershipResult) {}
 
-    abstract fun messageAction(pubnub: PubNub, pnMessageActionResult: PNMessageActionResult)
+    open fun messageAction(pubnub: PubNub, pnMessageActionResult: PNMessageActionResult) {}
 }
