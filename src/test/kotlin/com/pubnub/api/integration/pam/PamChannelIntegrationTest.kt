95167beae39c77d0f126cc1e5ea261933c034648
azurqsd <azur.haljeta@qsd.ba>
2020-06-18 17:22:37 +0200

    Add Grant API integration tests

---
+++ src/test/kotlin/com/pubnub/api/integration/pam/PamChannelIntegrationTest.kt
@@ -0,0 +1,6 @@
+package com.pubnub.api.integration.pam
+
+class PamChannelIntegrationTest : AccessManagerIntegrationTest() {
+
+    override fun getPamLevel() = LEVEL_CHANNEL
+}
---
+++ src/test/kotlin/com/pubnub/api/integration/pam/PamUserIntegrationTest.kt
@@ -0,0 +1,6 @@
+package com.pubnub.api.integration.pam
+
+class PamUserIntegrationTest : AccessManagerIntegrationTest() {
+
+    override fun getPamLevel() = LEVEL_USER
+}
