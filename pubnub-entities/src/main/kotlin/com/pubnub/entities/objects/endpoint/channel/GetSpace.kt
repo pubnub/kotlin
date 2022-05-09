3a24ea0e57b727f792afb5497b4979303470a687
Lukasz Klich <lukasz.klich@pubnub.com>
2022-05-09 16:56:23 +0200

    Create objectsService only once

--- pubnub-entities/src/main/kotlin/com/pubnub/entities/objects/endpoint/channel/GetSpace.kt
+++ pubnub-entities/src/main/kotlin/com/pubnub/entities/objects/endpoint/channel/GetSpace.kt
@@ -10,6 +10,7 @@ import com.pubnub.entities.objects.space.PNSpaceResult
 import com.pubnub.api.models.server.objects_api.EntityEnvelope
 import retrofit2.Call
 import retrofit2.Response
+import retrofit2.Retrofit
 import retrofit2.create
 import retrofit2.http.GET
 import retrofit2.http.Path
@@ -22,17 +23,22 @@ class GetSpace internal constructor(
     pubnub: PubNub,
     private val channel: String,
     private val withCustom: ReturningCustom
-) : Endpoint<EntityEnvelope<PNSpace>, PNSpaceResult>(pubnub, pubnub.telemetryManager, pubnub.retrofitManager, pubnub.tokenManager) {
+) : Endpoint<EntityEnvelope<PNSpace>, PNSpaceResult>(
+    pubnub,
+    pubnub.telemetryManager,
+    pubnub.retrofitManager,
+    pubnub.tokenManager
+) {
     override fun getAffectedChannels(): List<String?> = listOf(channel)
 
-    override fun getAffectedChannelGroups(): List<String?>  = listOf()
+    override fun getAffectedChannelGroups(): List<String?> = listOf()
 
     override fun validateParams() {
     }
 
     override fun doWork(queryParams: MutableMap<String, String>): Call<EntityEnvelope<PNSpace>> {
         val params = queryParams + withCustom.createIncludeQueryParams()
-        return pubnub.retrofitManager.objectsService.getChannelMetadata(
+        return pubnub.retrofitManager.objectsService().getChannelMetadata(
             subKey = pubnub.configuration.subscribeKey,
             channel = channel,
             options = params
@@ -51,7 +57,6 @@ class GetSpace internal constructor(
             )
         }
     }
-
 }
 
 interface ObjectsService {
@@ -63,5 +68,18 @@ interface ObjectsService {
     ): Call<EntityEnvelope<PNSpace>>
 }
 
-val RetrofitManager.objectsService: ObjectsService
-    get() = this.transactionInstance.create()
+object Lazy {
+    private lateinit var retrofit: Retrofit
+    private val objectsService: ObjectsService by lazy {
+        retrofit.create()
+    }
+
+    fun objectsService(retrofit: Retrofit): ObjectsService {
+        this.retrofit = retrofit
+        return objectsService
+    }
+}
+
+fun RetrofitManager.objectsService(): ObjectsService {
+    return Lazy.objectsService(this.transactionInstance)
+}
