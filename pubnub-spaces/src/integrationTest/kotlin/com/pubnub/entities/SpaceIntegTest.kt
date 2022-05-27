d63759e3b865392a7fb59b06be0c81165393fd2b
marcin.cebo <marcin.cebo@pubnub.com>
2022-05-27 16:24:29 +0200

    Fixes after code review

--- pubnub-spaces/src/integrationTest/kotlin/com/pubnub/entities/SpaceIntegTest.kt
+++ pubnub-spaces/src/integrationTest/kotlin/com/pubnub/entities/SpaceIntegTest.kt
@@ -12,7 +12,6 @@ import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 
-
 class SpaceIntegTest() {
     private lateinit var pubnub: PubNub
 
