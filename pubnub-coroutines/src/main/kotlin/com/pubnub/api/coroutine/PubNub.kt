9e35ac36d56ad7cb6e8db72b4497b74cc1a4e4f8
Lukasz Klich <lukasz.klich@pubnub.com>
2022-10-19 11:04:25 +0200

    Do something

--- pubnub-coroutines/src/main/kotlin/com/pubnub/api/coroutine/PubNub.kt
+++ pubnub-coroutines/src/main/kotlin/com/pubnub/api/coroutine/PubNub.kt
@@ -1,6 +1,14 @@
 package com.pubnub.api.coroutine
 
-class PubNub {
+import com.pubnub.api.Endpoint
+import com.pubnub.api.PNConfiguration
+import com.pubnub.api.models.consumer.PNStatus
+import kotlin.coroutines.resume
+import kotlin.coroutines.suspendCoroutine
+
+class PubNub(configuration: PNConfiguration) {
+
+    private val oldPubNub = com.pubnub.api.PubNub(configuration)
 
     suspend fun publish(
         channel: String,
@@ -10,7 +18,23 @@ class PubNub {
         usePost: Boolean = false,
         replicate: Boolean = true,
         ttl: Int? = null
-    ): Result<Long> {
-        TODO()
-    }
+    ): Result<Long> =
+        oldPubNub.publish(
+            channel = channel,
+            message = message,
+            meta = meta,
+            shouldStore = shouldStore,
+            usePost = usePost,
+            replicate = replicate,
+            ttl = ttl
+        ).coroutineResult().map { it.timetoken }
+
+    suspend fun <Input, Output> Endpoint<Input, Output>.coroutineResult(): Result<Output> =
+        suspendCoroutine { continuation ->
+            val callback = { result: Output?, status: PNStatus ->
+                if (status.error) continuation.resume(Result.failure(status.exception!!))
+                else continuation.resume(Result.success(result!!))
+            }
+            async(callback)
+        }
 }
