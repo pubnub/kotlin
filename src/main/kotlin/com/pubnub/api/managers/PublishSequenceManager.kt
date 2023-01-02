c942444457cb3b2c0fd63eb75e9e561190bb44a0
Lukasz Klich <kleewho@users.noreply.github.com>
2023-01-02 15:36:27 +0100

    Use atomic to have sequence without synchronization on Int (#96)

--- src/main/kotlin/com/pubnub/api/managers/PublishSequenceManager.kt
+++ src/main/kotlin/com/pubnub/api/managers/PublishSequenceManager.kt
@@ -1,17 +1,16 @@
 package com.pubnub.api.managers
 
+import java.util.concurrent.atomic.AtomicInteger
+
 internal class PublishSequenceManager(private val maxSequence: Int) {
 
-    private var nextSequence = 0
+    private val atomicSeq = AtomicInteger(1)
 
-    internal fun nextSequence(): Int {
-        synchronized(nextSequence) {
-            if (maxSequence == nextSequence) {
-                nextSequence = 1
-            } else {
-                nextSequence++
-            }
-            return nextSequence
+    internal fun nextSequence(): Int = atomicSeq.getAndUpdate {
+        if (maxSequence == it) {
+            1
+        } else {
+            it + 1
         }
     }
 }
