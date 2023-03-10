97e9c482f596e548cb2cf3a76d5f400ade56e68e
Lukasz Klich <lukasz.klich@pubnub.com>
2023-03-10 14:28:27 +0100

    Add body logging in test

--- pubnub-java-test/src/test/java/com/pubnub/TestTestTest.java
+++ pubnub-java-test/src/test/java/com/pubnub/TestTestTest.java
@@ -3,6 +3,7 @@ package com.pubnub;
 import com.pubnub.api.PNConfiguration;
 import com.pubnub.api.PubNub;
 import com.pubnub.api.PubNubException;
+import com.pubnub.api.enums.PNLogVerbosity;
 import com.pubnub.user.models.consumer.UsersResult;
 import org.junit.Test;
 
@@ -13,6 +14,7 @@ public class TestTestTest {
     @Test
     public void test() throws PubNubException {
         PubNub pubnub = new PubNub(new PNConfiguration("aaa")
+                .setLogVerbosity(PNLogVerbosity.BODY)
                 .setSubscribeKey(TestConfiguration.instance.getSubscribeKey())
                 .setPublishKey(TestConfiguration.instance.getPublishKey()));
 
