56215d5986524a1c8e9ff4a2ce710a727312d510
marcin.cebo <marcin.cebo@pubnub.com>
2023-05-18 17:15:36 +0200

    Changes after code review.

--- src/test/kotlin/com/pubnub/api/subscribe/eventengine/effect/HandshakeReconnectEffectTest.kt
+++ src/test/kotlin/com/pubnub/api/subscribe/eventengine/effect/HandshakeReconnectEffectTest.kt
@@ -46,15 +46,7 @@ class HandshakeReconnectEffectTest {
             .with()
             .pollInterval(Duration.ofMillis(20))
             .untilAsserted {
-                assertEquals(
-                    listOf(
-                        Event.HandshakeReconnectSuccess(
-                            channels,
-                            channelGroups,
-                            subscriptionCursor
-                        )
-                    ), eventSink.events
-                )
+                assertEquals(listOf(Event.HandshakeReconnectSuccess(channels, channelGroups, subscriptionCursor)), eventSink.events)
             }
     }
 
