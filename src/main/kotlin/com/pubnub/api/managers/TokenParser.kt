45dc541b08289d781625ccf0f39651d4ff9863b8
Lukasz Klich <lukasz.klich@pubnub.com>
2023-07-26 16:17:55 +0200

    Make token parsing more informative about errors

--- src/main/kotlin/com/pubnub/api/managers/TokenParser.kt
+++ src/main/kotlin/com/pubnub/api/managers/TokenParser.kt
@@ -2,10 +2,13 @@ package com.pubnub.api.managers
 
 import co.nstant.`in`.cbor.CborDecoder
 import co.nstant.`in`.cbor.model.ByteString
+import co.nstant.`in`.cbor.model.NegativeInteger
+import co.nstant.`in`.cbor.model.UnsignedInteger
 import com.pubnub.api.PubNubError
 import com.pubnub.api.PubNubException
 import com.pubnub.api.models.consumer.access_manager.v3.PNToken
 import com.pubnub.api.vendor.Base64
+import java.math.BigInteger
 import java.nio.charset.StandardCharsets
 import kotlin.collections.Map
 import kotlin.collections.component1
@@ -17,21 +20,45 @@ internal class TokenParser {
 
     fun unwrapToken(token: String): PNToken {
         val byteArray = Base64.decode(token.toByteArray(StandardCharsets.UTF_8), Base64.URL_SAFE)
-        val dataItems = CborDecoder(byteArray.inputStream()).decode()
-        return (dataItems.firstOrNull() as? CborMap)?.let {
-            val firstLevelMap = it.toJvmMap()
-            val resourcesValue = firstLevelMap[RESOURCES_KEY] as? Map<*, *>
-            val patternsValue = firstLevelMap[PATTERNS_KEY] as? Map<*, *>
+        val firstElement = CborDecoder(byteArray.inputStream()).decode().firstOrNull() ?: throw PubNubException(
+            pubnubError = PubNubError.INVALID_ACCESS_TOKEN, errorMessage = "Empty token"
+        )
+        val firstLevelMap = (firstElement as? CborMap)?.toJvmMap() ?: throw PubNubException(
+            pubnubError = PubNubError.INVALID_ACCESS_TOKEN, errorMessage = "First element is not a map"
+        )
+        val version = firstLevelMap[VERSION_KEY]?.toString()?.toInt() ?: throw PubNubException(
+            pubnubError = PubNubError.INVALID_ACCESS_TOKEN, errorMessage = "Couldn't parse version"
+        )
+        val timestamp = firstLevelMap[TIMESTAMP_KEY]?.toString()?.toLong() ?: throw PubNubException(
+            pubnubError = PubNubError.INVALID_ACCESS_TOKEN, errorMessage = "Couldn't parse timestamp"
+        )
+        val ttl = firstLevelMap[TTL_KEY]?.toString()?.toLong() ?: throw PubNubException(
+            pubnubError = PubNubError.INVALID_ACCESS_TOKEN, errorMessage = "Couldn't parse ttl"
+        )
+        val resourcesValue = firstLevelMap[RESOURCES_KEY] as? Map<*, *> ?: throw PubNubException(
+            pubnubError = PubNubError.INVALID_ACCESS_TOKEN, errorMessage = "Resources are not present or are not map"
+        )
+        val patternsValue = firstLevelMap[PATTERNS_KEY] as? Map<*, *> ?: throw PubNubException(
+            pubnubError = PubNubError.INVALID_ACCESS_TOKEN, errorMessage = "Patterns are not present or are not map"
+        )
+
+        return try {
+
             PNToken(
-                version = firstLevelMap[V_KEY].toString().toInt(),
-                timestamp = firstLevelMap[TIMESTAMP_KEY].toString().toLong(),
-                ttl = firstLevelMap[TTL_KEY].toString().toLong(),
+                version = version,
+                timestamp = timestamp,
+                ttl = ttl,
                 authorizedUUID = firstLevelMap[AUTHORIZED_UUID_KEY].toString(),
                 resources = resourcesValue.toPNTokenResources(),
                 patterns = patternsValue.toPNTokenResources(),
                 meta = firstLevelMap[META_KEY]
             )
-        }!!
+        } catch (e: Exception) {
+            if (e is PubNubException) throw e
+            throw PubNubException(
+                pubnubError = PubNubError.INVALID_ACCESS_TOKEN, errorMessage = "Couldn't parse token: ${e.message}"
+            )
+        }
     }
 
     private fun CborMap.toJvmMap(depth: Int = 0): MutableMap<String, Any> {
@@ -39,8 +66,8 @@ internal class TokenParser {
             throw PubNubException(pubnubError = PubNubError.INVALID_ACCESS_TOKEN, errorMessage = "Token is too deep")
         }
         val result = mutableMapOf<String, Any>()
-        for (key in keys) {
-            val value = get(key)
+        for (key in this.keys) {
+            val value = this.get(key)
             val keyString = when (key) {
                 is ByteString -> key.bytes.toString(StandardCharsets.UTF_8)
                 else -> key.toString()
@@ -50,29 +77,37 @@ internal class TokenParser {
                 is CborMap -> result[keyString] = value.toJvmMap(depth + 1)
                 is ByteString -> result[keyString] = value.bytes
                 is List<*> -> result[keyString] = value.map { it.toString() }
+                is UnsignedInteger -> result[keyString] = value.value
+                is NegativeInteger -> result[keyString] = value.value
                 else -> result[keyString] = value.toString()
             }
         }
         return result
     }
 
-    private fun Map<*, *>?.toPNTokenResources(): PNToken.PNTokenResources {
-        val channels = this?.get(CHANNELS_KEY) as? Map<*, *>
-        val groups = this?.get(GROUPS_KEY) as? Map<*, *>
-        val uuids = this?.get(UUIDS_KEY) as? Map<*, *>
+    private fun Map<*, *>.toMapOfStringToInt(): Map<String, Int> {
+        return mapNotNull { (k, v) ->
+            when (v) {
+                is BigInteger -> k.toString() to v.toInt()
+                else -> v.toString().toIntOrNull()?.let { k.toString() to it }
+            }
+        }.toMap()
+    }
+
+    private fun Map<*, *>.toPNTokenResources(): PNToken.PNTokenResources {
+        val channels = (this[CHANNELS_KEY] as? Map<*, *>)?.toMapOfStringToInt() ?: emptyMap()
+        val groups = (this[GROUPS_KEY] as? Map<*, *>)?.toMapOfStringToInt() ?: emptyMap()
+        val uuids = (this[UUIDS_KEY] as? Map<*, *>)?.toMapOfStringToInt() ?: emptyMap()
 
         return PNToken.PNTokenResources(
-            channels = channels?.map { (k, v) -> k.toString() to PNToken.PNResourcePermissions(v.toString().toInt()) }
-                ?.toMap() ?: emptyMap(),
-            channelGroups = groups?.map { (k, v) -> k.toString() to PNToken.PNResourcePermissions(v.toString().toInt()) }
-                ?.toMap() ?: emptyMap(),
-            uuids = uuids?.map { (k, v) -> k.toString() to PNToken.PNResourcePermissions(v.toString().toInt()) }
-                ?.toMap() ?: emptyMap(),
+            channels = channels.mapValues { (_, v) -> PNToken.PNResourcePermissions(v) },
+            channelGroups = groups.mapValues { (_, v) -> PNToken.PNResourcePermissions(v) },
+            uuids = uuids.mapValues { (_, v) -> PNToken.PNResourcePermissions(v) }
         )
     }
 
     companion object {
-        private const val V_KEY = "v"
+        private const val VERSION_KEY = "v"
         private const val TIMESTAMP_KEY = "t"
         private const val TTL_KEY = "ttl"
         private const val AUTHORIZED_UUID_KEY = "uuid"
--- src/test/kotlin/com/pubnub/contract/subscribe/eventEngine/state/TestSinkSource.kt
+++ src/test/kotlin/com/pubnub/contract/subscribe/eventEngine/state/TestSinkSource.kt
@@ -1,5 +1,6 @@
 package com.pubnub.contract.subscribe.eventEngine.state
 
+import com.fasterxml.jackson.databind.PropertyNamingStrategy.SnakeCaseStrategy
 import com.pubnub.api.eventengine.EffectInvocation
 import com.pubnub.api.eventengine.Event
 import com.pubnub.api.eventengine.QueueSinkSource
@@ -11,6 +12,7 @@ class TestSinkSource<T>(
     private val sinkSource: QueueSinkSource<T> = QueueSinkSource()
 ) : Sink<T>, Source<T> by sinkSource {
 
+    private val snakeCaseStrategy: SnakeCaseStrategy = SnakeCaseStrategy()
     override fun add(item: T) {
         testSink.add(item.type() to item.name())
         sinkSource.add(item)
@@ -18,7 +20,7 @@ class TestSinkSource<T>(
 
     private fun T.name() = this!!::class.simpleName!!.toSnakeCase().uppercase()
 
-    private fun String.toSnakeCase(): String = this
+    private fun String.toSnakeCase(): String = snakeCaseStrategy.translate(this)
 
     private fun T.type(): String = when (this) {
         is Event -> "event"
