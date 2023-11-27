b088374a80f2ff896a6568b36ee1c0748eef8d4e
Wojciech Kaliciński <wojciech.kalicinski@pubnub.com>
2023-11-27 17:44:12 +0100

    Fix tests

--- src/test/kotlin/com/pubnub/extension/JsonElementTest.kt
+++ src/test/kotlin/com/pubnub/extension/JsonElementTest.kt
@@ -5,6 +5,7 @@ import com.google.gson.JsonArray
 import com.google.gson.JsonElement
 import com.google.gson.JsonObject
 import com.google.gson.JsonPrimitive
+import com.pubnub.api.PubNubError
 import com.pubnub.api.crypto.CryptoModule
 import com.pubnub.api.managers.MapperManager
 import org.junit.jupiter.api.Assertions.assertEquals
@@ -94,7 +95,7 @@ class JsonElementTest {
 
         // then
         assertEquals(objectUnderTest, jsonElement)
-        assertEquals("Crypto is configured but message is not encrypted.", errorMessage?.message)
+        assertEquals(PubNubError.CRYPTO_IS_CONFIGURED_BUT_MESSAGE_IS_NOT_ENCRYPTED, errorMessage)
     }
 
     @ParameterizedTest
@@ -137,7 +138,7 @@ class JsonElementTest {
 
         // then
         assertEquals(objectUnderTest, jsonElement)
-        assertEquals("Crypto is configured but message is not encrypted.", errorMessage?.message)
+        assertEquals(PubNubError.CRYPTO_IS_CONFIGURED_BUT_MESSAGE_IS_NOT_ENCRYPTED, errorMessage)
     }
 
     @ParameterizedTest
