aaf9bc32c6fb88d3ca93ec0d0596b01153ffe528
Wojciech Kaliciński <wojciech.kalicinski@pubnub.com>
2024-06-04 14:06:41 +0200

    delete unneeded codable

--- pubnub-kotlin/pubnub-kotlin-api/src/commonMain/kotlin/com/pubnub/kmp/Codable.kt
+++
@@ -1,9 +0,0 @@
-package com.pubnub.kmp
-
-interface Codable {
-    /**
-     * This must return a type that is already considered Codable in Swift, such as
-     * Map, Collection, Array, String, Boolean, Int/Long. //todo what about null?
-     */
-    fun toCodable(): Any
-}
