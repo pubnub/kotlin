10902f2320b063f2da6f58024d4022a773fe42c6
Wojciech Kaliciński <wojciech.kalicinski@pubnub.com>
2024-06-04 16:26:45 +0200

    [WIP] Modify PushPayloadHelper to support FCM format

--- pubnub-core/pubnub-core-api/src/main/kotlin/com/pubnub/api/models/consumer/push/payload/PushPayloadHelper.kt
+++ pubnub-core/pubnub-core-api/src/main/kotlin/com/pubnub/api/models/consumer/push/payload/PushPayloadHelper.kt
@@ -1,6 +1,7 @@
 package com.pubnub.api.models.consumer.push.payload
 
 import com.pubnub.api.enums.PNPushEnvironment
+import java.beans.Visibility
 import java.util.Locale
 
 class PushPayloadHelper {
@@ -21,7 +22,7 @@ class PushPayloadHelper {
             fcmPayload?.let {
                 it.toMap().run {
                     if (isNotEmpty()) {
-                        put("pn_gcm", this)
+                        put("pn_fcm", this)
                     }
                 }
             }
@@ -127,9 +128,210 @@ class PushPayloadHelper {
     }
 
     class FCMPayload : PushPayloadSerializer {
-        var custom: Map<String, Any>? = null
         var data: Map<String, Any>? = null
         var notification: Notification? = null
+        var android: AndroidConfig? = null
+        var webpush: WebpushConfig? = null
+
+        class WebpushConfig : PushPayloadSerializer {
+            var headers: Map<String, String>? = null
+            var data: Map<String, String>? = null
+            var notification: Map<String, Any>? = null
+            var fcm_options: WebpushFcmOptions? = null
+
+            class WebpushFcmOptions : PushPayloadSerializer {
+                var link: String? = null
+                var analytics_label: String? = null
+
+                override fun toMap(): Map<String, Any> {
+                    TODO("Not yet implemented")
+                }
+            }
+
+            override fun toMap(): Map<String, Any> {
+                TODO("Not yet implemented")
+            }
+
+        }
+
+        var apns: ApnsConfig? = null
+
+        class ApnsConfig : PushPayloadSerializer {
+            var headers: Map<String, String>? = null
+            var payload: Map<String, String>? = null
+            var fcm_options: ApnsFcmOptions? = null
+
+            class ApnsFcmOptions : PushPayloadSerializer {
+                var analytics_label: String? = null
+                var image: String? = null
+
+                override fun toMap(): Map<String, Any> {
+                    TODO("Not yet implemented")
+                }
+
+            }
+
+            override fun toMap(): Map<String, Any> {
+                TODO("Not yet implemented")
+            }
+
+        }
+
+        var fcm_options: FcmOptions? = null
+
+        class FcmOptions: PushPayloadSerializer {
+            var analytics_label: String? = null
+
+            override fun toMap(): Map<String, Any> {
+                TODO("Not yet implemented")
+            }
+
+        }
+
+        var token: String? = null
+            set(value) {
+                topic = null
+                condition = null
+                field = value
+            }
+        var topic: String? = null
+            set(value) {
+                token = null
+                condition = null
+                field = value
+            }
+        var condition: String? = null
+            set(value) {
+                topic = null
+                token = null
+                field = value
+            }
+
+        class AndroidConfig : PushPayloadSerializer {
+            var collapseKey: String? = null
+            var priority: AndroidMessagePriority = AndroidMessagePriority.NORMAL
+            var ttl: String? = null
+            var restrictedPackageName: String? = null
+            var data: Map<String, Any>? = null
+            var notification: AndroidNotification? = null
+            var fcmOptions: AndroidFcmOptions? = null
+            var directBootOk: Boolean = false
+
+            class AndroidFcmOptions : PushPayloadSerializer {
+                var analytics_label: String? = null
+                override fun toMap(): Map<String, Any> {
+                    return buildMap {
+                        analytics_label?.let { put("analytics_label", it) }
+                    }
+                }
+            }
+
+            class AndroidNotification : PushPayloadSerializer {
+                var title: String? = null
+                var body: String? = null
+                var icon: String? = null
+                var color: String? = null
+                var sound: String? = null
+                var tag: String? = null
+                var body_loc_key: String? = null
+                var body_loc_args: List<String>? = null
+                var title_loc_key: String? = null
+                var title_loc_args: List<String>? = null
+                var channel_id: String? = null
+                var ticker: String? = null
+                var sticky: Boolean = false
+                var event_time: String? = null
+                var local_only: Boolean? = null
+                var notification_priority: NotificationPriority = NotificationPriority.PRIORITY_DEFAULT
+                var default_sound: Boolean? = null
+                var default_vibrate_timings: Boolean? = null
+                var default_light_settings: Boolean? = null
+                var vibrate_timings: List<String>? = null
+                var visibility: Visibility? = null
+                var notification_count: Int? = null
+                var light_settings: LightSettings? = null
+                var image: String? = null
+
+                class LightSettings : PushPayloadSerializer {
+                    //                    var color : {
+//                        "red": number,
+//                        "green": number,
+//                        "blue": number,
+//                        "alpha": number
+//                    }
+                    var light_on_duration: String? = null
+                    var light_off_duration: String? = null
+
+                    override fun toMap(): Map<String, Any> {
+                        TODO("Not yet implemented")
+                    }
+
+                }
+
+                enum class Visibility {
+                    VISIBILITY_UNSPECIFIED,
+                    PRIVATE,
+                    PUBLIC,
+                    SECRET,
+                }
+
+                enum class NotificationPriority {
+                    PRIORITY_UNSPECIFIED,
+                    PRIORITY_MIN,
+                    PRIORITY_LOW,
+                    PRIORITY_DEFAULT,
+                    PRIORITY_HIGH,
+                    PRIORITY_MAX
+                }
+
+                override fun toMap(): Map<String, Any> =
+                    buildMap {
+                        title?.let { put("title", it) }
+                        body?.let { put("body", it) }
+                        icon?.let { put("icon", it) }
+                        color?.let { put("color", it) }
+                        sound?.let { put("sound", it) }
+                        tag?.let { put("tag", it) }
+                        body_loc_key?.let { put("body_loc_key", it) }
+                        body_loc_args?.let { put("body_loc_args", it) }
+                        title_loc_key?.let { put("title_loc_key", it) }
+                        title_loc_args?.let { put("title_loc_args", it) }
+                        channel_id?.let { put("channel_id", it) }
+                        ticker?.let { put("ticker", it) }
+                        put("sticky", sticky)
+                        event_time?.let { put("event_time", it) }
+                        local_only?.let { put("local_only", it) }
+                        put("notification_priority", notification_priority.name)
+                        default_sound?.let { put("default_sound", it) }
+                        default_vibrate_timings?.let { put("default_vibrate_timings", it) }
+                        default_light_settings?.let { put("default_light_settings", it) }
+                        vibrate_timings?.let { put("vibrate_timings", it) }
+                        visibility?.let { put("visibility", it.name) }
+                        notification_count?.let { put("notification_count", it) }
+                        light_settings?.let { put("light_settings", it.toMap()) }
+                        image?.let { put("image", it) }
+                    }
+
+            }
+
+
+            enum class AndroidMessagePriority {
+                NORMAL, HIGH
+            }
+
+            override fun toMap(): Map<String, Any> {
+                return buildMap {
+                    collapseKey?.let { put("collapse_key", it) }
+                    put("priority", priority.name)
+                    ttl?.let { put("ttl", it) }
+                    restrictedPackageName?.let { put("restricted_package_name", it) }
+                    data?.let { put("data", it) }
+                    notification?.let { put("notification", it.toMap()) }
+                    fcmOptions?.let { put("fcm_options", it.toMap()) }
+                    put("direct_boot_ok", directBootOk)
+                }
+            }
+        }
 
         override fun toMap(): Map<String, Any> {
             return mutableMapOf<String, Any>().apply {
