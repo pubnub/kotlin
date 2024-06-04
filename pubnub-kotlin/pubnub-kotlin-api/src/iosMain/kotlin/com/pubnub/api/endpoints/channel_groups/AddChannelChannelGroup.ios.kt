c80d72c3920bf932c8c63b7647f61cbcfd8efd07
jguz-pubnub <102806147+jguz-pubnub@users.noreply.github.com>
2024-06-04 10:53:19 +0200

    Wrapping next Swift endpoints (#224)

--- pubnub-kotlin/pubnub-kotlin-api/src/iosMain/kotlin/com/pubnub/api/endpoints/channel_groups/AddChannelChannelGroup.ios.kt
+++ pubnub-kotlin/pubnub-kotlin-api/src/iosMain/kotlin/com/pubnub/api/endpoints/channel_groups/AddChannelChannelGroup.ios.kt
@@ -1,10 +1,33 @@
 package com.pubnub.api.endpoints.channel_groups
 
+import cocoapods.PubNubSwift.PubNubObjC
+import cocoapods.PubNubSwift.addChannelsTo
 import com.pubnub.api.Endpoint
 import com.pubnub.api.models.consumer.channel_group.PNChannelGroupsAddChannelResult
+import com.pubnub.api.onFailureHandler
+import com.pubnub.api.onSuccessHandler
+import com.pubnub.api.v2.callbacks.Consumer
+import com.pubnub.api.v2.callbacks.Result
+import kotlinx.cinterop.ExperimentalForeignApi
 
 /**
  * @see [PubNub.addChannelsToChannelGroup]
  */
 actual interface AddChannelChannelGroup : Endpoint<PNChannelGroupsAddChannelResult> {
+}
+
+@OptIn(ExperimentalForeignApi::class)
+class AddChannelChannelGroupImpl(
+    private val pubnub: PubNubObjC,
+    private val channels: List<String>,
+    private val channelGroup: String
+): AddChannelChannelGroup {
+    override fun async(callback: Consumer<Result<PNChannelGroupsAddChannelResult>>) {
+        pubnub.addChannelsTo(
+            channelGroup = channelGroup,
+            channels = channels,
+            onSuccess = callback.onSuccessHandler { PNChannelGroupsAddChannelResult() },
+            onFailure = callback.onFailureHandler()
+        )
+    }
 }
--- pubnub-kotlin/pubnub-kotlin-api/src/iosMain/kotlin/com/pubnub/api/endpoints/presence/WhereNow.ios.kt
+++ pubnub-kotlin/pubnub-kotlin-api/src/iosMain/kotlin/com/pubnub/api/endpoints/presence/WhereNow.ios.kt
@@ -1,10 +1,31 @@
 package com.pubnub.api.endpoints.presence
 
+import cocoapods.PubNubSwift.PubNubObjC
+import cocoapods.PubNubSwift.whereNowWithUuid
 import com.pubnub.api.Endpoint
 import com.pubnub.api.models.consumer.presence.PNWhereNowResult
+import com.pubnub.api.onFailureHandler
+import com.pubnub.api.onSuccessHandler
+import com.pubnub.api.v2.callbacks.Consumer
+import com.pubnub.api.v2.callbacks.Result
+import kotlinx.cinterop.ExperimentalForeignApi
 
 /**
  * @see [PubNub.whereNow]
  */
 actual interface WhereNow : Endpoint<PNWhereNowResult> {
+}
+
+@OptIn(ExperimentalForeignApi::class)
+class WhereNowImpl(
+    public val pubnub: PubNubObjC,
+    public val uuid: String
+): WhereNow {
+    override fun async(callback: Consumer<Result<PNWhereNowResult>>) {
+        pubnub.whereNowWithUuid(
+            uuid = uuid,
+            onSuccess = callback.onSuccessHandler { PNWhereNowResult(it as? List<String> ?: emptyList()) },
+            onFailure = callback.onFailureHandler()
+        )
+    }
 }
