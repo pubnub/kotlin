5ef78480a10deb2279f9724b69b951e04057844b
Wojciech Kaliciński <wojciech.kalicinski@pubnub.com>
2024-06-18 16:07:17 +0200

    Alias c.p.kmp.PubNub and import c.p.api.PubNub

--- pubnub-kotlin/pubnub-kotlin-api/src/jvmMain/kotlin/com/pubnub/kmp/factories.jvm.kt
+++ pubnub-kotlin/pubnub-kotlin-api/src/jvmMain/kotlin/com/pubnub/kmp/factories.jvm.kt
@@ -10,53 +10,55 @@ import com.pubnub.api.models.consumer.pubsub.objects.PNObjectEventResult
 import com.pubnub.api.v2.PNConfiguration
 import com.pubnub.api.v2.callbacks.EventListener
 import com.pubnub.api.v2.callbacks.StatusListener
+import com.pubnub.api.PubNub
+import com.pubnub.kmp.PubNub as PubNubKmp
 
-actual fun createPubNub(config: PNConfiguration): PubNub {
-    return com.pubnub.api.PubNub.create(config)
+actual fun createPubNub(config: PNConfiguration): PubNubKmp {
+    return PubNub.create(config)
 }
 
 actual fun createEventListener(
-    pubnub: PubNub,
-    onMessage: (PubNub, PNMessageResult) -> Unit,
-    onPresence: (PubNub, PNPresenceEventResult) -> Unit,
-    onSignal: (PubNub, PNSignalResult) -> Unit,
-    onMessageAction: (PubNub, PNMessageActionResult) -> Unit,
-    onObjects: (PubNub, PNObjectEventResult) -> Unit,
-    onFile: (PubNub, PNFileEventResult) -> Unit
+    pubnub: PubNubKmp,
+    onMessage: (PubNubKmp, PNMessageResult) -> Unit,
+    onPresence: (PubNubKmp, PNPresenceEventResult) -> Unit,
+    onSignal: (PubNubKmp, PNSignalResult) -> Unit,
+    onMessageAction: (PubNubKmp, PNMessageActionResult) -> Unit,
+    onObjects: (PubNubKmp, PNObjectEventResult) -> Unit,
+    onFile: (PubNubKmp, PNFileEventResult) -> Unit
 ): EventListener {
     return object : EventListener {
-        override fun message(pubnub: com.pubnub.api.PubNub, result: PNMessageResult) {
+        override fun message(pubnub: PubNub, result: PNMessageResult) {
             onMessage(pubnub, result)
         }
 
-        override fun presence(pubnub: com.pubnub.api.PubNub, result: PNPresenceEventResult) {
+        override fun presence(pubnub: PubNub, result: PNPresenceEventResult) {
             onPresence(pubnub, result)
         }
 
-        override fun signal(pubnub: com.pubnub.api.PubNub, result: PNSignalResult) {
+        override fun signal(pubnub: PubNub, result: PNSignalResult) {
             onSignal(pubnub, result)
         }
 
-        override fun messageAction(pubnub: com.pubnub.api.PubNub, result: PNMessageActionResult) {
+        override fun messageAction(pubnub: PubNub, result: PNMessageActionResult) {
             onMessageAction(pubnub, result)
         }
 
-        override fun objects(pubnub: com.pubnub.api.PubNub, result: PNObjectEventResult) {
+        override fun objects(pubnub: PubNub, result: PNObjectEventResult) {
             onObjects(pubnub, result)
         }
 
-        override fun file(pubnub: com.pubnub.api.PubNub, result: PNFileEventResult) {
+        override fun file(pubnub: PubNub, result: PNFileEventResult) {
             onFile(pubnub, result)
         }
     }
 }
 
 actual fun createStatusListener(
-    pubnub: PubNub,
-    onStatus: (PubNub, PNStatus) -> Unit
+    pubnub: PubNubKmp,
+    onStatus: (PubNubKmp, PNStatus) -> Unit
 ): StatusListener {
     return object : StatusListener {
-        override fun status(pubnub: com.pubnub.api.PubNub, status: PNStatus) {
+        override fun status(pubnub: PubNub, status: PNStatus) {
             onStatus(pubnub, status)
         }
     }
