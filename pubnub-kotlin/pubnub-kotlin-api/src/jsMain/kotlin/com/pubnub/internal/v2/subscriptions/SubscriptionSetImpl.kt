dc79c264a15cf26bf52e03308875f562a8569bc0
Wojtek Kaliciński <146713236+wkal-pubnub@users.noreply.github.com>
2024-06-28 09:37:04 +0000

    SubscriptionSetImpl in JS and PNFuture.remember() (#246)

--- pubnub-kotlin/pubnub-kotlin-api/src/jsMain/kotlin/com/pubnub/internal/v2/subscriptions/SubscriptionSetImpl.kt
+++ pubnub-kotlin/pubnub-kotlin-api/src/jsMain/kotlin/com/pubnub/internal/v2/subscriptions/SubscriptionSetImpl.kt
@@ -12,58 +12,66 @@ import com.pubnub.api.v2.subscriptions.Subscription
 import com.pubnub.api.v2.subscriptions.SubscriptionCursor
 import com.pubnub.api.v2.subscriptions.SubscriptionSet
 
-class SubscriptionImpl(private val jsSubscription: dynamic) : Subscription {
-
+class SubscriptionSetImpl(private val jsSubscriptionSet: dynamic) : SubscriptionSet {
     override fun close() {
         unsubscribe()
     }
 
-    override fun plus(subscription: Subscription): SubscriptionSet {
+    override fun plusAssign(subscription: Subscription) {
         TODO("Not yet implemented")
     }
 
+    override fun minusAssign(subscription: Subscription) {
+        TODO("Not yet implemented")
+    }
+
+    override fun add(subscription: Subscription) {
+        TODO("Not yet implemented")
+    }
+
+    override fun remove(subscription: Subscription) {
+        TODO("Not yet implemented")
+    }
+
+    override val subscriptions: Set<Subscription>
+        get() = TODO("Not yet implemented")
+
     override fun addListener(listener: EventListener) {
-        jsSubscription.addListener(listener)
+        jsSubscriptionSet.addListener(listener)
     }
 
     override fun removeListener(listener: Listener) {
-        jsSubscription.removeListener(listener)
+        jsSubscriptionSet.removeListener(listener)
     }
 
     override fun removeAllListeners() {
-        TODO("Not yet implemented") //todo js doesn't support this?
+        TODO("Not yet implemented")
     }
 
-    override fun subscribe(cursor: SubscriptionCursor) { // todo use cursor
-        jsSubscription.subscribe()
+    override fun subscribe(cursor: SubscriptionCursor) {
+        jsSubscriptionSet.subscribe()
     }
 
     override fun unsubscribe() {
-        jsSubscription.unsubscribe()
+        jsSubscriptionSet.unsubscribe()
     }
 
     override var onMessage: ((PNMessageResult) -> Unit)?
         get() = TODO("Not yet implemented")
         set(value) {}
-
     override var onPresence: ((PNPresenceEventResult) -> Unit)?
         get() = TODO("Not yet implemented")
         set(value) {}
-
     override var onSignal: ((PNSignalResult) -> Unit)?
         get() = TODO("Not yet implemented")
         set(value) {}
-
     override var onMessageAction: ((PNMessageActionResult) -> Unit)?
         get() = TODO("Not yet implemented")
         set(value) {}
-
     override var onObjects: ((PNObjectEventResult) -> Unit)?
         get() = TODO("Not yet implemented")
         set(value) {}
-
     override var onFile: ((PNFileEventResult) -> Unit)?
         get() = TODO("Not yet implemented")
         set(value) {}
-
 }
