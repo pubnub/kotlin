929e248693d0e1b60c684f66d473cf7a446e3de8
marcin.cebo <marcin.cebo@pubnub.com>
2024-10-01 18:25:45 +0200

    Changes after review.

--- pubnub-kotlin/pubnub-kotlin-api/src/commonTest/kotlin/com/pubnub/api/util/TimetokenUtilsTest.kt
+++ pubnub-kotlin/pubnub-kotlin-api/src/commonTest/kotlin/com/pubnub/api/util/TimetokenUtilsTest.kt
@@ -2,7 +2,6 @@ package com.pubnub.api.util
 
 import com.pubnub.api.utils.TimetokenUtil
 import kotlinx.datetime.Instant
-import kotlinx.datetime.LocalDateTime
 import kotlinx.datetime.TimeZone
 import kotlinx.datetime.toLocalDateTime
 import kotlin.test.Test
@@ -15,27 +14,29 @@ class TimetokenUtilsTest {
         val timetoken = 17276954606232118
 
         // when
-        val timetokenToDateTimeUTC: LocalDateTime = TimetokenUtil.timetokenToDateTimeUTC(timetoken)
-        val timetokenResult = TimetokenUtil.dateTimeUTCToTimetoken(timetokenToDateTimeUTC)
+        val instant: Instant = TimetokenUtil.timetokenToInstant(timetoken)
+        val timetokenResult = TimetokenUtil.instantToTimetoken(instant)
 
         // then
-        assertEquals("2024-09-30", timetokenToDateTimeUTC.date.toString())
-        assertEquals("11:24:20.623211800", timetokenToDateTimeUTC.time.toString())
+        val localDateTimeInUTC = instant.toLocalDateTime(TimeZone.UTC)
+        assertEquals("2024-09-30", localDateTimeInUTC.date.toString())
+        assertEquals("11:24:20.623211800", localDateTimeInUTC.time.toString())
         assertEquals(timetoken, timetokenResult)
     }
 
     @Test
-    fun can_convert_unitTime_to_timetoken() {
+    fun can_convert_unixTime_in_seconds_to_timetoken() {
         // given 2024-09-30 20:00:12
         val unixTime = 1727726412.toLong()
 
         // when
-        val timetoken = TimetokenUtil.unixToTimetoken(unixTime)
+        val timetoken: Long = TimetokenUtil.unixToTimetoken(unixTime)
 
         // then
-        val dateTimeUTC: LocalDateTime = TimetokenUtil.timetokenToDateTimeUTC(timetoken)
-        assertEquals("2024-09-30", dateTimeUTC.date.toString())
-        assertEquals("20:00:12", dateTimeUTC.time.toString())
+        val instant: Instant = TimetokenUtil.timetokenToInstant(timetoken)
+        val localDateTimeInUTC = instant.toLocalDateTime(TimeZone.UTC)
+        assertEquals("2024-09-30", localDateTimeInUTC.date.toString())
+        assertEquals("20:00:12", localDateTimeInUTC.time.toString())
     }
 
     @Test
@@ -47,9 +48,9 @@ class TimetokenUtilsTest {
         val unixTime = TimetokenUtil.timetokenToUnix(timetoken)
 
         // then
-        val instant = Instant.fromEpochSeconds(unixTime)
+        val instant = Instant.fromEpochMilliseconds(unixTime)
         val toLocalDateTime = instant.toLocalDateTime(TimeZone.UTC)
         assertEquals("2024-09-30", toLocalDateTime.date.toString())
-        assertEquals("11:24:20", toLocalDateTime.time.toString())
+        assertEquals("11:24:20.623", toLocalDateTime.time.toString())
     }
 }
--- pubnub-kotlin/pubnub-kotlin-core-api/src/commonMain/kotlin/com/pubnub/api/utils/TimetokenUtil.kt
+++ pubnub-kotlin/pubnub-kotlin-core-api/src/commonMain/kotlin/com/pubnub/api/utils/TimetokenUtil.kt
@@ -1,14 +1,13 @@
 package com.pubnub.api.utils
 
 import kotlinx.datetime.Instant
-import kotlinx.datetime.LocalDateTime
-import kotlinx.datetime.TimeZone
-import kotlinx.datetime.toInstant
-import kotlinx.datetime.toLocalDateTime
+import kotlin.math.pow
 
 private const val MINIMA_TIMETOKE_VALUE = 10_000_000_000_000_000
 
 private const val MAXIMUM_TIMETOKEN_VALUE = 99_999_999_999_999_999
+
+private const val NUMBER_OF_DIGITS_IN_TIMETOKEN = 17
 
 /**
  * Utility object for converting PubNub timetokens to various date-time representations and vice versa.
@@ -25,10 +24,10 @@ object TimetokenUtil {
      * the corresponding date and time.
      *
      * @param timetoken PubNub timetoken
-     * @return [LocalDateTime] representing the corresponding data and time in UTC.
+     * @return [Instant] representing the corresponding data and time.
      * @throws IllegalArgumentException if the timetoken does not have 17 digits.
      */
-    fun timetokenToDateTimeUTC(timetoken: Long): LocalDateTime {
+    fun timetokenToInstant(timetoken: Long): Instant {
         if (isLengthDifferentThan17Digits(timetoken)) {
             throw IllegalArgumentException("Timetoken should have 17 digits")
         }
@@ -37,20 +36,18 @@ object TimetokenUtil {
         val epochNanoseconds =
             ((timetoken % 10_000_000) * 100).toInt() // The remainder, multiplied by 100 to get nanoseconds
 
-        val instant = Instant.fromEpochSeconds(epochSeconds, epochNanoseconds)
-        return instant.toLocalDateTime(TimeZone.UTC)
+        return Instant.fromEpochSeconds(epochSeconds, epochNanoseconds)
     }
 
     /**
-     * Converts [LocalDateTime] in UTC to a PubNub timetoken
+     * Converts [Instant] to a PubNub timetoken
      *
      * A PubNub timetoken is a 17-digit number representing the number of 100-nanosecond intervals since January 1, 1970.
      *
-     * @param dateTime The [LocalDateTime] object in UTC to be converted to a PubNub timetoken.
-     * @return A 17-digit [Long] representing the PubNub timetoken for the given [LocalDateTime].
+     * @param instant The [Instant] object to be converted to a PubNub timetoken.
+     * @return A 17-digit [Long] representing the PubNub timetoken for the given [Instant].
      */
-    fun dateTimeUTCToTimetoken(dataTime: LocalDateTime): Long {
-        val instant = dataTime.toInstant(TimeZone.UTC)
+    fun instantToTimetoken(instant: Instant): Long {
         val epochSeconds: Long = instant.epochSeconds
         val epochNanoseconds: Int = instant.nanosecondsOfSecond
 
@@ -61,30 +58,29 @@ object TimetokenUtil {
     }
 
     /**
-     * Converts a Unix timestamp (in seconds)to a PubNub timetoken
+     * Converts a Unix timestamp to a PubNub timetoken
      *
-     * A Unix timestamp represents the number of seconds that have elapsed since January 1, 1970 (midnight UTC/GMT).
-     * This function converts the Unix timestamp, which must be provided in seconds, to a PubNub timetoken.
-     *
-     * @param unixTime The Unix timestamp in seconds to be converted to a PubNub timetoken.
+     * @param unixTime The Unix timestamp to be converted to a PubNub timetoken.
      * @return A 17-digit [Long] representing the PubNub timetoken corresponding to the given Unix timestamp.
      */
     fun unixToTimetoken(unixTime: Long): Long {
-        return (unixTime * 10_000_000)
+        val numberOfDigits = unixTime.toString().length
+        val zerosToBeAdded = NUMBER_OF_DIGITS_IN_TIMETOKEN - numberOfDigits
+        return unixTime * 10.toDouble().pow(zerosToBeAdded).toLong()
     }
 
     /**
-     * Converts a PubNub timetoken to a Unix timestamp (in seconds).
+     * Converts a PubNub timetoken to a Unix timestamp (in millis).
      *
      * A PubNub timetoken is a 17-digit number representing the number of 100-nanosecond intervals since January 1, 1970.
-     * This function converts the PubNub timetoken to a Unix timestamp by reducing the precision to seconds.
-     * Note that precision finer than seconds is lost in this conversion.
+     * This function converts the PubNub timetoken to a Unix timestamp by reducing the precision to millis.
+     * Note that precision finer than millis is lost in this conversion.
      *
      * @param timetoken The PubNub timetoken to be converted to a Unix timestamp.
-     * @return A [Long] representing the Unix timestamp in seconds corresponding to the given timetoken.
+     * @return A [Long] representing the Unix timestamp in millis corresponding to the given timetoken.
      */
     fun timetokenToUnix(timetoken: Long): Long {
-        return (timetoken / 10_000_000)
+        return (timetoken / 10_000)
     }
 
     private fun isLengthDifferentThan17Digits(timetoken: Long): Boolean {
