8ad63278b63da3621c6bf95d632ea8dbd9d48618
Wojciech Kaliciński <wojciech.kalicinski@pubnub.com>
2024-10-08 16:24:56 +0200

    Test ipv6+http2

--- build-logic/gradle-plugins/src/main/kotlin/com/pubnub/gradle/PubNubKotlinLibraryPlugin.kt
+++ build-logic/gradle-plugins/src/main/kotlin/com/pubnub/gradle/PubNubKotlinLibraryPlugin.kt
@@ -2,8 +2,11 @@ package com.pubnub.gradle
 
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.tasks.testing.AbstractTestTask
+import org.gradle.api.tasks.testing.logging.TestExceptionFormat
 import org.gradle.kotlin.dsl.apply
 import org.gradle.kotlin.dsl.configure
+import org.gradle.kotlin.dsl.withType
 import org.jetbrains.kotlin.gradle.dsl.KotlinJvmProjectExtension
 import org.jetbrains.kotlin.gradle.plugin.KotlinPluginWrapper
 import org.jetbrains.kotlin.gradle.tasks.KotlinJvmCompile
@@ -24,6 +27,18 @@ class PubNubKotlinLibraryPlugin : Plugin<Project> {
                     javaParameters.set(true)
                     freeCompilerArgs.add("-Xjvm-default=all")
                 }
+            }
+
+            tasks.withType<AbstractTestTask> {
+                testLogging {
+                    it.showStackTraces = true
+                    it.showCauses = true
+                    it.showExceptions = true
+                    it.exceptionFormat = TestExceptionFormat.FULL
+                }
+//                if (providers.environmentVariable("CI").isPresent && !providers.environmentVariable("CI_FORCE_RUN_INTEGRATION_TESTS").isPresent) {
+//                    filter.excludeTestsMatching("com.pubnub.test.integration.*")
+//                }
             }
 
         }
