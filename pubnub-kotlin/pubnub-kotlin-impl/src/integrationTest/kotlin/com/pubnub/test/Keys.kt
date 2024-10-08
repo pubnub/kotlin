e6de5fe43c6dae7bf4a6d084104291aaeeee87d1
Wojciech Kaliciński <wojciech.kalicinski@pubnub.com>
2024-10-08 16:35:38 +0200

    Test ipv6+http2

--- pubnub-kotlin/pubnub-kotlin-impl/src/integrationTest/kotlin/com/pubnub/test/Keys.kt
+++ pubnub-kotlin/pubnub-kotlin-impl/src/integrationTest/kotlin/com/pubnub/test/Keys.kt
@@ -3,21 +3,21 @@ package com.pubnub.test
 import org.aeonbits.owner.Config
 import org.aeonbits.owner.ConfigFactory
 
-@Config.Sources("file:../../test.properties")
+@Config.Sources("system:env", "file:../../test.properties")
 interface KeysConfig : Config {
-    @get:Config.Key("subKey")
+    @get:Config.Key("SDK_SUB_KEY")
     val subKey: String
 
-    @get:Config.Key("pubKey")
+    @get:Config.Key("SDK_PUB_KEY")
     val pubKey: String
 
-    @get:Config.Key("pamSubKey")
+    @get:Config.Key("SDK_PAM_SUB_KEY")
     val pamSubKey: String
 
-    @get:Config.Key("pamPubKey")
+    @get:Config.Key("SDK_PAM_PUB_KEY")
     val pamPubKey: String
 
-    @get:Config.Key("pamSecKey")
+    @get:Config.Key("SDK_PAM_SEC_KEY")
     val pamSecKey: String
 }
 
