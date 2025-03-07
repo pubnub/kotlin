d1df97567378063c9261544e4d7463078e700190
marcin.cebo <marcin.cebo@pubnub.com>
2025-03-07 11:29:09 +0100

    changes after review

--- pubnub-kotlin/pubnub-kotlin-coroutines/src/test/kotlin/com/pubnub/coroutines/UtilKtTest.kt
+++ pubnub-kotlin/pubnub-kotlin-coroutines/src/test/kotlin/com/pubnub/coroutines/UtilKtTest.kt
@@ -1,7 +1,5 @@
 package com.pubnub.coroutines
 
-import com.pubnub.api.v2.callbacks.Consumer
-import com.pubnub.kmp.PNFuture
 import com.pubnub.kmp.asFuture
 import com.pubnub.kmp.then
 import kotlinx.coroutines.test.runTest
@@ -9,34 +7,18 @@ import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 import kotlin.test.assertFailsWith
 
-class FakePNFuture<T>(private val block: (Consumer<com.pubnub.api.v2.callbacks.Result<T>>) -> Unit) : PNFuture<T> {
-    override fun async(callback: Consumer<com.pubnub.api.v2.callbacks.Result<T>>) {
-        block(callback)
-    }
-}
-
 class UtilKtTest {
     @Test
     fun `await returns result on success`() = runTest {
         val expected = "Success Result"
-        val future = FakePNFuture<String> { callback ->
-            // Simulate a successful asynchronous result.
-            callback.accept(com.pubnub.api.v2.callbacks.Result.success(expected))
-        }
-
-        // When await() is called, it should resume with the expected result.
-        val result = future.await()
-        assertEquals(expected, result)
+        assertEquals(expected, expected.asFuture().await())
     }
 
     @Test
     fun `await throws exception on failure`() = runTest {
         val exception = Exception("Failure occurred")
-//        val future = FakePNFuture<String> { callback ->
-//            // Simulate an asynchronous failure.
-//            callback.accept(com.pubnub.api.v2.callbacks.Result.failure(exception))
-//        }
-        val future = Unit.asFuture().then { error("exception!") }
+
+        val future = exception.asFuture().then { throw it }
         // When await() is called, it should resume with an exception.
         val thrown = assertFailsWith<Exception> {
             future.await()
