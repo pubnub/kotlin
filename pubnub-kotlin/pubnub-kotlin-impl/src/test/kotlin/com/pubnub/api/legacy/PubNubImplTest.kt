a5a565b3692bb30e0dbaf7915cfe136db5becd7a
jguz-pubnub <102806147+jguz-pubnub@users.noreply.github.com>
2025-05-13 23:21:42 +0200

    Update PubNub Swift SDK dependency to 9.1.0 (#346)
    * Update PubNub Swift SDK dependency to 9.1.0
    * PubNub SDK v10.5.1 release
    
    ---------
    
    Co-authored-by: PubNub Release Bot <120067856+pubnub-release-bot@users.noreply.github.com>

--- pubnub-kotlin/pubnub-kotlin-impl/src/test/kotlin/com/pubnub/api/legacy/PubNubImplTest.kt
+++ pubnub-kotlin/pubnub-kotlin-impl/src/test/kotlin/com/pubnub/api/legacy/PubNubImplTest.kt
@@ -56,7 +56,7 @@ class PubNubImplTest : BaseTest() {
     fun getVersionAndTimeStamp() {
         val version = PubNubImpl.SDK_VERSION
         val timeStamp = PubNubImpl.timestamp()
-        assertEquals("10.5.0", version)
+        assertEquals("10.5.1", version)
         assertTrue(timeStamp > 0)
     }
 
