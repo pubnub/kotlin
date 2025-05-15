19960450291f013f55865fc7b7979720ccc5b037
marcin.cebo <marcin.cebo@pubnub.com>
2025-05-15 18:43:40 +0200

    Added missing snippet

--- pubnub-gson/pubnub-gson-docs/src/main/java/com/pubnub/docs/accessManager/GrantTokenApp.java
+++ pubnub-gson/pubnub-gson-docs/src/main/java/com/pubnub/docs/accessManager/GrantTokenApp.java
@@ -1,18 +1,18 @@
 package com.pubnub.docs.accessManager;
+// https://www.pubnub.com/docs/sdks/java/api-reference/access-manager#basic-usage
 
+// snippet.basicGrantToken
 import com.pubnub.api.PubNubException;
 import com.pubnub.api.UserId;
 import com.pubnub.api.java.PubNub;
 import com.pubnub.api.java.models.consumer.access_manager.v3.ChannelGrant;
 import com.pubnub.api.java.v2.PNConfiguration;
+import com.pubnub.api.models.consumer.access_manager.v3.PNGrantTokenResult;
 
 import java.util.Arrays;
 
 public class GrantTokenApp {
     public static void main(String[] args) throws PubNubException {
-        // https://www.pubnub.com/docs/sdks/java/api-reference/access-manager#basic-usage
-
-        // snippet.basicGrantToken
         PNConfiguration.Builder configBuilder = PNConfiguration.builder(new UserId("demoUserId"), "demo");
         configBuilder.publishKey("demo");
         configBuilder.secretKey("yourSecretKey");  // <-- a VALID secretKey is required
@@ -24,7 +24,13 @@ public class GrantTokenApp {
                 .channels(Arrays.asList(
                         ChannelGrant.name("channel-a").read(),
                         ChannelGrant.name("channel-b").read().write()))
-                .async(result -> { /* check result */ });
-        // snippet.end
+                .async(result -> {
+                    result.onSuccess((PNGrantTokenResult grantTokenResult) -> {
+                        System.out.println("Token: " + grantTokenResult.getToken());
+                    }).onFailure((PubNubException exception) -> {
+                        System.out.println("Error granting token: " + exception.getMessage());
+                    });
+                });
     }
 }
+// snippet.end
