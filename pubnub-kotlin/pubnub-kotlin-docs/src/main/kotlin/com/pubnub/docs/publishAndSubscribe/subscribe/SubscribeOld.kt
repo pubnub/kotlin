0004bdf7b910afdab4b6010f7a4243fdb4cb9af8
Mateusz Wiktor <mateusz.wiktor@pubnub.com>
2025-05-28 09:27:38 +0200

    added simple subscribe snippet

--- pubnub-kotlin/pubnub-kotlin-docs/src/main/kotlin/com/pubnub/docs/publishAndSubscribe/subscribe/SubscribeOld.kt
+++ pubnub-kotlin/pubnub-kotlin-docs/src/main/kotlin/com/pubnub/docs/publishAndSubscribe/subscribe/SubscribeOld.kt
@@ -28,6 +28,19 @@ class SubscribeOld : SnippetBase() {
         // snippet.end
     }
 
+    private fun subscribe() {
+        // https://www.pubnub.com/docs/sdks/kotlin/api-reference/publish-and-subscribe#basic-usage-1
+
+        val pubnub = createPubNub()
+
+        // snippet.subscribe
+        pubnub.subscribe(
+            channels = listOf("my_channel")
+        )
+
+        // snippet.end
+    }
+
     private fun subscribeWithMultipleChannels() {
         // https://www.pubnub.com/docs/sdks/kotlin/api-reference/publish-and-subscribe#subscribing-to-multiple-channels
 
