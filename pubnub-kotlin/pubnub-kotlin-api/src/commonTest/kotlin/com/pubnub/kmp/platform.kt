428e381d05cc6be8a8fd829aede755ee081690e3
marcin.cebo <marcin.cebo@pubnub.com>
2025-06-04 17:27:37 +0200

    Added test for sendFile

--- pubnub-kotlin/pubnub-kotlin-api/src/commonTest/kotlin/com/pubnub/kmp/platform.kt
+++ pubnub-kotlin/pubnub-kotlin-api/src/commonTest/kotlin/com/pubnub/kmp/platform.kt
@@ -1,3 +1,7 @@
 package com.pubnub.kmp
 
 internal expect val PLATFORM: String
+
+internal expect fun stringToUploadable(content: String, contentType: String? = null): Uploadable
+
+internal expect fun readAllBytes(stream: Any?): ByteArray
--- pubnub-kotlin/pubnub-kotlin-api/src/jvmTest/kotlin/com/pubnub/kmp/platform.jvm.kt
+++ pubnub-kotlin/pubnub-kotlin-api/src/jvmTest/kotlin/com/pubnub/kmp/platform.jvm.kt
@@ -1,3 +1,13 @@
 package com.pubnub.kmp
 
+import java.io.ByteArrayInputStream
+import java.io.InputStream
+
 internal actual val PLATFORM: String = "JVM"
+
+internal actual fun stringToUploadable(content: String, contentType: String?): Uploadable =
+    ByteArrayInputStream(content.toByteArray())
+
+internal actual fun readAllBytes(stream: Any?): ByteArray {
+    return (stream as InputStream).readBytes()
+}
