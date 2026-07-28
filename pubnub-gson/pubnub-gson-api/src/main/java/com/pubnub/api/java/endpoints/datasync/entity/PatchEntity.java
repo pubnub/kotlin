40c81cfee17772955d1bf3cddf711fedc5abb3e0
marcin.cebo <marcin.cebo@pubnub.com>
2026-07-28 15:01:44 +0200

    Lint

--- pubnub-gson/pubnub-gson-api/src/main/java/com/pubnub/api/java/endpoints/datasync/entity/PatchEntity.java
+++ pubnub-gson/pubnub-gson-api/src/main/java/com/pubnub/api/java/endpoints/datasync/entity/PatchEntity.java
@@ -1,11 +1,8 @@
 package com.pubnub.api.java.endpoints.datasync.entity;
 
 import com.pubnub.api.java.endpoints.Endpoint;
-import com.pubnub.api.java.models.consumer.datasync.entity.PNJsonPatchOperation;
 import com.pubnub.api.java.models.consumer.datasync.entity.PNPatchEntityResult;
 import org.jetbrains.annotations.Nullable;
-
-import java.util.List;
 
 /**
  * @see com.pubnub.api.java.datasync.EntityApi#patch(String, List)
